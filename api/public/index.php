456392352ebf29ef88fce21bb35dea90147a61f2
ElisDN <mail@elisdn.ru>
2019-12-19 22:16:33 +0300

    Installed Slim

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,17 @@
 
 declare(strict_types=1);
 
-header('Content-Type: application/json');
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-echo '{}';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('{}');
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
