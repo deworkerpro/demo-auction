4e7854baee86219c86cc7ad1edbd8a7599c245c8
ElisDN <mail@elisdn.ru>
2019-12-19 22:20:06 +0300

    Fixed server error status code

--- api/public/index.php
+++ api/public/index.php
@@ -6,6 +6,8 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+http_response_code(500);
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
