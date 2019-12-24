e6ba55e26ea7c2a9312942873431004df0126c28
ElisDN <mail@elisdn.ru>
2019-12-24 19:20:26 +0300

    Rewrite to PSR request handler

--- api/src/Http/Action/HomeAction.php
+++ api/src/Http/Action/HomeAction.php
@@ -5,14 +5,24 @@ declare(strict_types=1);
 namespace App\Http\Action;
 
 use App\Http;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use stdClass;
 
-class HomeAction
+class HomeAction implements RequestHandlerInterface
 {
-    public function __invoke(Request $request, Response $response, $args): Response
+    private ResponseFactoryInterface $factory;
+
+    public function __construct(ResponseFactoryInterface $factory)
     {
+        $this->factory = $factory;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $response = $this->factory->createResponse();
         return Http::json($response, new stdClass());
     }
 }
