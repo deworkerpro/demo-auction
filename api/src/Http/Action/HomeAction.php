1dece2725f9a5a0c0c74ea7a68929c1516d44087
ElisDN <mail@elisdn.ru>
2019-12-24 19:23:20 +0300

    Added JSON response

--- api/src/Http/Action/HomeAction.php
+++ api/src/Http/Action/HomeAction.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Action;
 
-use App\Http;
-use Psr\Http\Message\ResponseFactoryInterface;
+use App\Http\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -13,16 +12,8 @@ use stdClass;
 
 class HomeAction implements RequestHandlerInterface
 {
-    private ResponseFactoryInterface $factory;
-
-    public function __construct(ResponseFactoryInterface $factory)
-    {
-        $this->factory = $factory;
-    }
-
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $response = $this->factory->createResponse();
-        return Http::json($response, new stdClass());
+        return new JsonResponse(new stdClass());
     }
 }
