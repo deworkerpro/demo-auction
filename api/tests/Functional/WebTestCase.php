3eb7fecd430e14c25d4f56d261147c31f071b2ba
ElisDN <mail@elisdn.ru>
2019-12-25 13:04:02 +0300

    Extracted web test case base class

---
+++ api/tests/Functional/WebTestCase.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+use Slim\Psr7\Factory\ServerRequestFactory;
+
+class WebTestCase extends TestCase
+{
+    protected static function json(string $method, string $path): ServerRequestInterface
+    {
+        return self::request($method, $path)
+            ->withHeader('Accept', 'application/json')
+            ->withHeader('Content-Type', 'application/json');
+    }
+
+    protected static function request(string $method, string $path): ServerRequestInterface
+    {
+        return (new ServerRequestFactory())->createServerRequest($method, $path);
+    }
+
+    protected function app(): App
+    {
+        /** @var App */
+        return (require __DIR__ . '/../../config/app.php')($this->container());
+    }
+
+    private function container(): ContainerInterface
+    {
+        /** @var ContainerInterface */
+        return require __DIR__ . '/../../config/container.php';
+    }
+}
