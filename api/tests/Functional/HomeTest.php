69e9a39366bc5c36773f3b9684c0f6e9343851b8
ElisDN <mail@elisdn.ru>
2019-12-25 13:04:02 +0300

    Extracted web test case base class

--- api/tests/Functional/HomeTest.php
+++ api/tests/Functional/HomeTest.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace Test\Functional;
 
-use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Slim\App;
-use Slim\Psr7\Factory\ServerRequestFactory;
-
-class HomeTest extends TestCase
+class HomeTest extends WebTestCase
 {
     public function testSuccess(): void
     {
@@ -19,25 +13,5 @@ class HomeTest extends TestCase
         self::assertEquals(200, $response->getStatusCode());
         self::assertEquals('application/json', $response->getHeaderLine('Content-Type'));
         self::assertEquals('{}', (string)$response->getBody());
-    }
-
-    private static function json(string $method, string $path): ServerRequestInterface
-    {
-        return self::request($method, $path)
-            ->withHeader('Accept', 'application/json')
-            ->withHeader('Content-Type', 'application/json');
-    }
-
-    private static function request(string $method, string $path): ServerRequestInterface
-    {
-        return (new ServerRequestFactory())->createServerRequest($method, $path);
-    }
-
-    private function app(): App
-    {
-        /** @var ContainerInterface $container */
-        $container = require __DIR__ . '/../../config/container.php';
-        /** @var App */
-        return (require __DIR__ . '/../../config/app.php')($container);
     }
 }
