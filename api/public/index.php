f1167dbf5371e9b26e489db9291cee2309d4d786
ElisDN <mail@elisdn.ru>
2019-12-26 16:57:39 +0300

    Extracted app bootstrap

--- api/public/index.php
+++ api/public/index.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
-use Slim\Factory\AppFactory;
+use Slim\App;
 
 http_response_code(500);
 
@@ -12,9 +12,6 @@ require __DIR__ . '/../vendor/autoload.php';
 /** @var ContainerInterface $container */
 $container = require __DIR__ . '/../config/container.php';
 
-$app = AppFactory::createFromContainer($container);
-
-(require __DIR__ . '/../config/middleware.php')($app, $container);
-(require __DIR__ . '/../config/routes.php')($app);
-
+/** @var App $app */
+$app = (require __DIR__ . '/../config/app.php')($container);
 $app->run();
