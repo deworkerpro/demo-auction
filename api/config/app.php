3e48299e82dcae69041f095dc0e7a8de4d5cc848
ElisDN <mail@elisdn.ru>
2019-12-26 16:57:39 +0300

    Extracted app bootstrap

---
+++ api/config/app.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Container\ContainerInterface;
+use Slim\App;
+use Slim\Factory\AppFactory;
+
+return static function (ContainerInterface $container): App {
+    $app = AppFactory::createFromContainer($container);
+    (require __DIR__ . '/middleware.php')($app, $container);
+    (require __DIR__ . '/routes.php')($app);
+    return $app;
+};
