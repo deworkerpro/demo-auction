d9a6add6898dc782a6bf083db2196ab31ae411c8
ElisDN <mail@elisdn.ru>
2019-12-26 16:46:37 +0300

    Extracted dependencies

--- api/public/index.php
+++ api/public/index.php
@@ -9,13 +9,7 @@ http_response_code(500);
 require __DIR__ . '/../vendor/autoload.php';
 
 $builder = new DI\ContainerBuilder();
-
-$builder->addDefinitions([
-    'config' => [
-        'debug' => (bool)getenv('APP_DEBUG'),
-    ],
-]);
-
+$builder->addDefinitions(require __DIR__ . '/../config/dependencies.php');
 $container = $builder->build();
 
 $app = AppFactory::createFromContainer($container);
