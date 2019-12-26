21f72502037a7d6034930cd5b3ccdbc3d3acf55f
ElisDN <mail@elisdn.ru>
2019-12-26 16:28:13 +0300

    Added DI configuration

--- api/public/index.php
+++ api/public/index.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -11,11 +10,19 @@ http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$container = new Container();
+$builder = new DI\ContainerBuilder();
+
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool)getenv('APP_DEBUG'),
+    ],
+]);
+
+$container = $builder->build();
 
 $app = AppFactory::createFromContainer($container);
 
-$app->addErrorMiddleware((bool)getenv('APP_DEBUG'), true, true);
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write('{}');
