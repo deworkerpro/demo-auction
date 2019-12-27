4f935267804b884160e440fead5ef34f20dc049b
ElisDN <mail@elisdn.ru>
2019-12-27 12:39:20 +0300

    Added configs autoloading

--- api/config/dependencies.php
+++ api/config/dependencies.php
@@ -2,8 +2,13 @@
 
 declare(strict_types=1);
 
-return [
-    'config' => [
-        'debug' => (bool)getenv('APP_DEBUG'),
-    ],
-];
+$files = glob(__DIR__ . '/common/*.php');
+
+$configs = array_map(
+    static function ($file) {
+        return require $file;
+    },
+    $files
+);
+
+return array_merge_recursive(...$configs);
