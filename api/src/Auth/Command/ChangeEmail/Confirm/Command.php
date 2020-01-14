f5be36f2e52e970af9f7f195e08ee3e549b86a98
ElisDN <mail@elisdn.ru>
2020-01-14 18:49:07 +0300

    Added user email change confirm

---
+++ api/src/Auth/Command/ChangeEmail/Confirm/Command.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Command\ChangeEmail\Confirm;
+
+class Command
+{
+    public string $token = '';
+}
---
+++ api/src/Auth/Command/ChangeEmail/Confirm/Handler.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Command\ChangeEmail\Confirm;
+
+use App\Auth\Entity\User\UserRepository;
+use App\Flusher;
+use DateTimeImmutable;
+use DomainException;
+
+class Handler
+{
+    private UserRepository $users;
+    private Flusher $flusher;
+
+    public function __construct(UserRepository $users, Flusher $flusher)
+    {
+        $this->users = $users;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        if (!$user = $this->users->findByNewEmailToken($command->token)) {
+            throw new DomainException('Incorrect token.');
+        }
+
+        $user->confirmEmailChanging(
+            $command->token,
+            new DateTimeImmutable()
+        );
+
+        $this->flusher->flush();
+    }
+}
