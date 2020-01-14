126f1884b0513d386248f367c7113b0c8341fa33
ElisDN <mail@elisdn.ru>
2020-01-14 17:43:30 +0300

    Added join by network use case

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -7,6 +7,7 @@ namespace App\Auth\Entity\User;
 interface UserRepository
 {
     public function hasByEmail(Email $email): bool;
+    public function hasByNetwork(NetworkIdentity $identity): bool;
     public function findByJoinConfirmToken(string $token): ?User;
     public function add(User $user): void;
 }
