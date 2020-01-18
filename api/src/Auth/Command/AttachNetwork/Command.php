bda62e372d16c50bb9bc4752d9ae1dd4296ab280
ElisDN <mail@elisdn.ru>
2020-01-18 12:11:24 +0300

    Added network attach use case

---
+++ api/src/Auth/Command/AttachNetwork/Command.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Command\AttachNetwork;
+
+class Command
+{
+    public string $id = '';
+    public string $network = '';
+    public string $identity = '';
+}
