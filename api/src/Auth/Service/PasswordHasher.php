9443d612578b2a92280b63c3f8d72c30913a4c02
ElisDN <mail@elisdn.ru>
2020-01-19 13:27:46 +0300

    Implemented password hasher

--- api/src/Auth/Service/PasswordHasher.php
+++ api/src/Auth/Service/PasswordHasher.php
@@ -4,8 +4,27 @@ declare(strict_types=1);
 
 namespace App\Auth\Service;
 
-interface PasswordHasher
+use RuntimeException;
+use Webmozart\Assert\Assert;
+
+class PasswordHasher
 {
-    public function hash(string $password): string;
-    public function validate(string $password, string $hash): bool;
+    public function hash(string $password): string
+    {
+        Assert::notEmpty($password);
+        /** @var string|false|null $hash */
+        $hash = password_hash($password, PASSWORD_ARGON2I);
+        if ($hash === null) {
+            throw new RuntimeException('Invalid hash algorithm.');
+        }
+        if ($hash === false) {
+            throw new RuntimeException('Unable to generate hash.');
+        }
+        return $hash;
+    }
+
+    public function validate(string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
 }
