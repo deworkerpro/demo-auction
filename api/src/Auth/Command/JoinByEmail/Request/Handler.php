e4c4476aae2168af1e8484bdbf1dc3c17dd77643
ElisDN <mail@elisdn.ru>
2020-01-19 13:25:25 +0300

    Added join request use case

--- api/src/Auth/Command/JoinByEmail/Request/Handler.php
+++ api/src/Auth/Command/JoinByEmail/Request/Handler.php
@@ -4,10 +4,61 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Request;
 
+use App\Auth\Entity\User\Email;
+use App\Auth\Entity\User\Id;
+use App\Auth\Entity\User\User;
+use App\Auth\Entity\User\UserRepository;
+use App\Auth\Service\JoinConfirmationSender;
+use App\Auth\Service\PasswordHasher;
+use App\Auth\Service\Tokenizer;
+use App\Flusher;
+use DateTimeImmutable;
+use DomainException;
+
 class Handler
 {
+    private UserRepository $users;
+    private PasswordHasher $hasher;
+    private Tokenizer $tokenizer;
+    private Flusher $flusher;
+    private JoinConfirmationSender $sender;
+
+    public function __construct(
+        UserRepository $users,
+        PasswordHasher $hasher,
+        Tokenizer $tokenizer,
+        Flusher $flusher,
+        JoinConfirmationSender $sender
+    ) {
+        $this->users = $users;
+        $this->hasher = $hasher;
+        $this->tokenizer = $tokenizer;
+        $this->flusher = $flusher;
+        $this->sender = $sender;
+    }
+
     public function handle(Command $command): void
     {
+        $email = new Email($command->email);
 
+        if ($this->users->hasByEmail($email)) {
+            throw new DomainException('User already exists.');
+        }
+
+        $date = new DateTimeImmutable();
+
+        $user = new User(
+            Id::generate(),
+            $date,
+            $email,
+            $this->hasher->hash($command->password),
+            $token = $this->tokenizer->generate($date)
+        );
+
+        $this->users->add($user);
+
+        $this->flusher->flush();
+
+        $this->sender->send($email, $token);
     }
 }
---
+++ api/src/Auth/Entity/User/Token.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Entity\User;
+
+use DateTimeImmutable;
+use Webmozart\Assert\Assert;
+
+class Token
+{
+    private string $value;
+    private DateTimeImmutable $expires;
+
+    public function __construct(string $value, DateTimeImmutable $expires)
+    {
+        Assert::uuid($value);
+        $this->value = mb_strtolower($value);
+        $this->expires = $expires;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function getExpires(): DateTimeImmutable
+    {
+        return $this->expires;
+    }
+}
