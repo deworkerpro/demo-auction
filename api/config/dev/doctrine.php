aaa7e170458d9d365688bb85c45e3290a89e0fa0
ElisDN <mail@elisdn.ru>
2020-02-14 20:01:54 +0300

    Added manager configuration

---
+++ api/config/dev/doctrine.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'config' => [
+        'doctrine' => [
+            'dev_mode' => true,
+            'cache_dir' => null,
+            'proxy_dir' => __DIR__ . '/../../var/cache/' . PHP_SAPI . '/doctrine/proxy',
+        ],
+    ],
+];
