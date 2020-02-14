f1ab012b0a271e9de9edbc7c4abbfc9ac34dd8c3
ElisDN <mail@elisdn.ru>
2020-02-14 20:05:06 +0300

    Splitted database console commands

--- api/config/common/console.php
+++ api/config/common/console.php
@@ -3,12 +3,14 @@
 declare(strict_types=1);
 
 use App\Console;
+use Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand;
 
 return [
     'config' => [
         'console' => [
             'commands' => [
                 Console\HelloCommand::class,
+                ValidateSchemaCommand::class,
             ],
         ],
     ],
