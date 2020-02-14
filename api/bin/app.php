82b697478a8775042d65bdda0e625525236e9176
ElisDN <mail@elisdn.ru>
2020-02-14 20:05:06 +0300

    Splitted database console commands

--- api/bin/app.php
+++ api/bin/app.php
@@ -26,8 +26,6 @@ $commands = $container->get('config')['console']['commands'];
 $entityManager = $container->get(EntityManagerInterface::class);
 $cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
 
-Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
-
 foreach ($commands as $name) {
     /** @var Command $command */
     $command = $container->get($name);
