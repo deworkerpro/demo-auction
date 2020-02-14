6c38c37a3123e229839946122989671fbb08365a
ElisDN <mail@elisdn.ru>
2020-02-14 20:55:37 +0300

    Added fixtures load command

--- api/config/dev/console.php
+++ api/config/dev/console.php
@@ -2,17 +2,41 @@
 
 declare(strict_types=1);
 
+use App\Console\FixturesLoadCommand;
 use Doctrine\Migrations;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Command\SchemaTool;
+use Psr\Container\ContainerInterface;
 
 return [
+    FixturesLoadCommand::class => static function (ContainerInterface $container) {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @psalm-var array{fixture_paths:string[]} $config
+         */
+        $config = $container->get('config')['console'];
+
+        /** @var EntityManagerInterface $em */
+        $em = $container->get(EntityManagerInterface::class);
+
+        return new FixturesLoadCommand(
+            $em,
+            $config['fixture_paths'],
+        );
+    },
+
     'config' => [
         'console' => [
             'commands' => [
+                FixturesLoadCommand::class,
+
                 SchemaTool\DropCommand::class,
 
                 Migrations\Tools\Console\Command\DiffCommand::class,
                 Migrations\Tools\Console\Command\GenerateCommand::class,
+            ],
+            'fixture_paths' => [
+                __DIR__ . '/../../src/Auth/Fixture',
             ],
         ],
     ],
