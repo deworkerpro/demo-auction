41c3db854aa512c53b30f51c42c2043f5b029786
ElisDN <mail@elisdn.ru>
2020-02-15 17:09:47 +0300

    Implemented user repository

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -4,27 +4,101 @@ declare(strict_types=1);
 
 namespace App\Auth\Entity\User;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 use DomainException;
 
-interface UserRepository
+class UserRepository
 {
-    public function hasByEmail(Email $email): bool;
-    public function hasByNetwork(Network $network): bool;
-    public function findByJoinConfirmToken(string $token): ?User;
-    public function findByPasswordResetToken(string $token): ?User;
-    public function findByNewEmailToken(string $token): ?User;
+    private EntityRepository $repo;
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        /** @var EntityRepository $repo */
+        $repo = $em->getRepository(User::class);
+        $this->repo = $repo;
+        $this->em = $em;
+    }
+
+    public function hasByEmail(Email $email): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.email = :email')
+                ->setParameter(':email', $email->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    public function hasByNetwork(Network $network): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->innerJoin('t.networks', 'n')
+                ->andWhere('n.network = :name and n.identity = :identity')
+                ->setParameter(':name', $network->getName())
+                ->setParameter(':identity', $network->getIdentity())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
     /**
-     * @param Id $id
-     * @return User
-     * @throws DomainException
+     * @param string $token
+     * @return User|object|null
+     * @psalm-return User|null
      */
-    public function get(Id $id): User;
+    public function findByJoinConfirmToken(string $token): ?User
+    {
+        /** @psalm-var User|null */
+        return $this->repo->findOneBy(['joinConfirmToken.value' => $token]);
+    }
+
     /**
-     * @param Email $email
-     * @return User
-     * @throws DomainException
+     * @param string $token
+     * @return User|object|null
+     * @psalm-return User|null
      */
-    public function getByEmail(Email $email): User;
-    public function add(User $user): void;
-    public function remove(User $user): void;
+    public function findByPasswordResetToken(string $token): ?User
+    {
+        /** @psalm-var User|null */
+        return $this->repo->findOneBy(['passwordResetToken.value' => $token]);
+    }
+
+    /**
+     * @param string $token
+     * @return User|object|null
+     * @psalm-return User|null
+     */
+    public function findByNewEmailToken(string $token): ?User
+    {
+        /** @psalm-var User|null */
+        return $this->repo->findOneBy(['newEmailToken.value' => $token]);
+    }
+
+    public function get(Id $id): User
+    {
+        if (!$user = $this->repo->find($id->getValue())) {
+            throw new DomainException('User is not found.');
+        }
+        /** @var User $user */
+        return $user;
+    }
+
+    public function getByEmail(Email $email): User
+    {
+        if (!$user = $this->repo->findOneBy(['email' => $email->getValue()])) {
+            throw new DomainException('User is not found.');
+        }
+        /** @var User $user */
+        return $user;
+    }
+
+    public function add(User $user): void
+    {
+        $this->em->persist($user);
+    }
+
+    public function remove(User $user): void
+    {
+        $this->em->remove($user);
+    }
 }
