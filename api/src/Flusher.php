b2b4731b347042a16998d1938d094fec6c9565a2
ElisDN <mail@elisdn.ru>
2020-02-15 17:09:58 +0300

    Implemented flusher

--- api/src/Flusher.php
+++ api/src/Flusher.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 
 namespace App;
 
-interface Flusher
+use Doctrine\ORM\EntityManagerInterface;
+
+class Flusher
 {
-    public function flush(): void;
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function flush(): void
+    {
+        $this->em->flush();
+    }
 }
