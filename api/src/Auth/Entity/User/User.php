c96fe47f93510f69f34d65b24ce0371cc0472324
ElisDN <mail@elisdn.ru>
2020-02-15 17:00:07 +0300

    Added token embedded mapping

--- api/src/Auth/Entity/User/User.php
+++ api/src/Auth/Entity/User/User.php
@@ -37,12 +37,21 @@ class User
      * @ORM\Column(type="auth_user_status", length=16)
      */
     private Status $status;
+    /**
+     * @ORM\Embedded(class="Token")
+     */
     private ?Token $joinConfirmToken = null;
+    /**
+     * @ORM\Embedded(class="Token")
+     */
     private ?Token $passwordResetToken = null;
     /**
      * @ORM\Column(type="auth_user_email", nullable=true)
      */
     private ?Email $newEmail = null;
+    /**
+     * @ORM\Embedded(class="Token")
+     */
     private ?Token $newEmailToken = null;
     /**
      * @ORM\Column(type="auth_user_role", length=16)
