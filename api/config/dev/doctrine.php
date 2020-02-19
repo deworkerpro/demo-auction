51d01b5375a85f3e38b8c556a21f79ac25e73413
ElisDN <mail@elisdn.ru>
2020-02-19 15:13:40 +0300

    Added schema fix subscriber

--- api/config/dev/doctrine.php
+++ api/config/dev/doctrine.php
@@ -2,12 +2,17 @@
 
 declare(strict_types=1);
 
+use App\Data\Doctrine\FixDefaultSchemaSubscriber;
+
 return [
     'config' => [
         'doctrine' => [
             'dev_mode' => true,
             'cache_dir' => null,
             'proxy_dir' => __DIR__ . '/../../var/cache/' . PHP_SAPI . '/doctrine/proxy',
+            'subscribers' => [
+                FixDefaultSchemaSubscriber::class,
+            ],
         ],
     ],
 ];
---
+++ api/src/Data/Doctrine/FixDefaultSchemaSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Doctrine;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\DBAL\Schema\PostgreSqlSchemaManager;
+use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+use Doctrine\ORM\Tools\ToolEvents;
+
+class FixDefaultSchemaSubscriber implements EventSubscriber
+{
+    public function getSubscribedEvents(): array
+    {
+        return [
+            ToolEvents::postGenerateSchema => 'postGenerateSchema'
+        ];
+    }
+
+    public function postGenerateSchema(GenerateSchemaEventArgs $args): void
+    {
+        $schemaManager = $args
+            ->getEntityManager()
+            ->getConnection()
+            ->getSchemaManager();
+
+        if (!$schemaManager instanceof PostgreSqlSchemaManager) {
+            return;
+        }
+
+        foreach ($schemaManager->getExistingSchemaSearchPaths() as $namespace) {
+            if (!$args->getSchema()->hasNamespace($namespace)) {
+                $args->getSchema()->createNamespace($namespace);
+            }
+        }
+    }
+}
