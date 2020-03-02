c0aa4e79fb86ffaabbff7eee7223f32f05abc861
ElisDN <mail@elisdn.ru>
2020-03-02 15:11:44 +0300

    Removed useless factory

--- api/config/common/auth.php
+++ api/config/common/auth.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use App\Auth;
 use App\Auth\Entity\User\User;
 use App\Auth\Entity\User\UserRepository;
-use App\Auth\Service\JoinConfirmationSender;
-use App\Frontend\FrontendUrlGenerator;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Psr\Container\ContainerInterface;
@@ -18,14 +16,5 @@ return [
         /** @var EntityRepository $repo */
         $repo = $em->getRepository(User::class);
         return new UserRepository($em, $repo);
-    },
-
-    JoinConfirmationSender::class => function (ContainerInterface $container): JoinConfirmationSender {
-        /** @var Swift_Mailer $mailer */
-        $mailer = $container->get(Swift_Mailer::class);
-        /** @var FrontendUrlGenerator $frontend */
-        $frontend = $container->get(FrontendUrlGenerator::class);
-
-        return new JoinConfirmationSender($mailer, $frontend);
     },
 ];
