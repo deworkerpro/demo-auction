f7018a38dc691b71c6449b7cb97007d9da9f9fa8
ElisDN <mail@elisdn.ru>
2020-03-02 12:26:41 +0300

    Added frontend url generator

--- api/config/common/frontend.php
+++ api/config/common/frontend.php
@@ -2,7 +2,20 @@
 
 declare(strict_types=1);
 
+use App\Frontend\FrontendUrlGenerator;
+use Psr\Container\ContainerInterface;
+
 return [
+    FrontendUrlGenerator::class => function (ContainerInterface $container): FrontendUrlGenerator {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @psalm-var array{url:string} $config
+         */
+        $config = $container->get('config')['frontend'];
+
+        return new FrontendUrlGenerator($config['url']);
+    },
+
     'config' => [
         'frontend' => [
             'url' => getenv('FRONTEND_URL'),
