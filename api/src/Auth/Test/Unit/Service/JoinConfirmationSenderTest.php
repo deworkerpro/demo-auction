082e9ffc1980f2079d59ef64ddba6feebc60ada1
ElisDN <mail@elisdn.ru>
2020-03-02 14:52:16 +0300

    Encapsulated email default sender

--- api/src/Auth/Test/Unit/Service/JoinConfirmationSenderTest.php
+++ api/src/Auth/Test/Unit/Service/JoinConfirmationSenderTest.php
@@ -21,36 +21,33 @@ class JoinConfirmationSenderTest extends TestCase
 {
     public function testSuccess(): void
     {
-        $from = ['test@app.test' => 'Test'];
         $to = new Email('user@app.test');
         $token = new Token(Uuid::uuid4()->toString(), new DateTimeImmutable());
         $confirmUrl = '/join/confirm?token=' . $token->getValue();
 
         $mailer = $this->createMock(Swift_Mailer::class);
         $mailer->expects($this->once())->method('send')
-            ->willReturnCallback(static function (Swift_Message $message) use ($from, $to, $confirmUrl): int {
-                self::assertEquals($from, $message->getFrom());
+            ->willReturnCallback(static function (Swift_Message $message) use ($to, $confirmUrl): int {
                 self::assertEquals([$to->getValue() => null], $message->getTo());
                 self::assertEquals('Join Confirmation', $message->getSubject());
                 self::assertStringContainsString($confirmUrl, $message->getBody());
                 return 1;
             });
 
-        $sender = new JoinConfirmationSender($mailer, $from);
+        $sender = new JoinConfirmationSender($mailer);
 
         $sender->send($to, $token);
     }
 
     public function testError(): void
     {
-        $from = ['test@app.test' => 'Test'];
         $to = new Email('user@app.test');
         $token = new Token(Uuid::uuid4()->toString(), new DateTimeImmutable());
 
         $mailer = $this->createStub(Swift_Mailer::class);
         $mailer->method('send')->willReturn(0);
 
-        $sender = new JoinConfirmationSender($mailer, $from);
+        $sender = new JoinConfirmationSender($mailer);
 
         $this->expectException(RuntimeException::class);
         $sender->send($to, $token);
