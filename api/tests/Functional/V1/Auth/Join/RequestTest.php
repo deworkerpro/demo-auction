694a035a856b8debf50ba610391f9f76e050ca43
ElisDN <mail@elisdn.ru>
2020-03-10 22:39:49 +0300

    Added locale exception test

--- api/tests/Functional/V1/Auth/Join/RequestTest.php
+++ api/tests/Functional/V1/Auth/Join/RequestTest.php
@@ -55,6 +55,25 @@ class RequestTest extends WebTestCase
         ], Json::decode($body));
     }
 
+    public function testExistingLang(): void
+    {
+        $this->markTestIncomplete('Waiting for translation.');
+
+        $response = $this->app()->handle(self::json('POST', '/v1/auth/join', [
+            'email' => 'existing@app.test',
+            'password' => 'new-password',
+        ])->withHeader('Accept-Language', 'ru'));
+
+        self::assertEquals(409, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        $data = Json::decode($body);
+
+        self::assertEquals([
+            'message' => 'Пользователь уже существует.',
+        ], $data);
+    }
+
     public function testEmpty(): void
     {
         $response = $this->app()->handle(self::json('POST', '/v1/auth/join', []));
