7ff55a3fff7947c069f82fc275a04637ca8a7e53
ElisDN <mail@elisdn.ru>
2020-03-10 16:36:52 +0300

    Added tokenizer factory

--- api/config/common/auth.php
+++ api/config/common/auth.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Auth;
 use App\Auth\Entity\User\User;
 use App\Auth\Entity\User\UserRepository;
+use App\Auth\Service\Tokenizer;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Psr\Container\ContainerInterface;
@@ -17,4 +18,20 @@ return [
         $repo = $em->getRepository(User::class);
         return new UserRepository($em, $repo);
     },
+
+    Tokenizer::class => function (ContainerInterface $container): Tokenizer {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @psalm-var array{token_ttl:string} $config
+         */
+        $config = $container->get('config')['auth'];
+
+        return new Tokenizer(new DateInterval($config['token_ttl']));
+    },
+
+    'config' => [
+        'auth' => [
+            'token_ttl' => 'PT1H',
+        ]
+    ],
 ];
