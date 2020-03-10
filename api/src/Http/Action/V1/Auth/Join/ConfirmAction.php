c8586e595a3b0fc556851b55b21246d5d4b27b3d
ElisDN <mail@elisdn.ru>
2020-03-10 20:46:13 +0300

    Extracted validation handling

--- api/src/Http/Action/V1/Auth/Join/ConfirmAction.php
+++ api/src/Http/Action/V1/Auth/Join/ConfirmAction.php
@@ -7,13 +7,10 @@ namespace App\Http\Action\V1\Auth\Join;
 use App\Auth\Command\JoinByEmail\Confirm\Command;
 use App\Auth\Command\JoinByEmail\Confirm\Handler;
 use App\Http\EmptyResponse;
-use App\Http\JsonResponse;
-use App\Http\Validator\ValidationException;
 use App\Http\Validator\Validator;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Symfony\Component\Validator\ConstraintViolationInterface;
 
 class ConfirmAction implements RequestHandlerInterface
 {
@@ -36,16 +33,7 @@ class ConfirmAction implements RequestHandlerInterface
         $command = new Command();
         $command->token = $data['token'] ?? '';
 
-        try {
-            $this->validator->validate($command);
-        } catch (ValidationException $exception) {
-            $errors = [];
-            /** @var ConstraintViolationInterface $violation */
-            foreach ($exception->getViolations() as $violation) {
-                $errors[$violation->getPropertyPath()] = $violation->getMessage();
-            }
-            return new JsonResponse(['errors' => $errors], 422);
-        }
+        $this->validator->validate($command);
 
         $this->handler->handle($command);
 
