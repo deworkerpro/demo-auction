c6545bcce58405696479275f998cebe4d21849ff
ElisDN <mail@elisdn.ru>
2020-03-11 19:15:01 +0300

    Added join confirm action

---
+++ api/src/Http/Action/V1/Auth/Join/ConfirmAction.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Action\V1\Auth\Join;
+
+use App\Auth\Command\JoinByEmail\Confirm\Command;
+use App\Auth\Command\JoinByEmail\Confirm\Handler;
+use App\Http\EmptyResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ConfirmAction implements RequestHandlerInterface
+{
+    private Handler $handler;
+
+    public function __construct(Handler $handler)
+    {
+        $this->handler = $handler;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        /**
+         * @psalm-var array{token:?string} $data
+         */
+        $data = $request->getParsedBody();
+
+        $command = new Command();
+        $command->token = $data['token'] ?? '';
+
+        $this->handler->handle($command);
+
+        return new EmptyResponse(200);
+    }
+}
---
+++ api/tests/Functional/V1/Auth/Join/ConfirmTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional\V1\Auth\Join;
+
+use Ramsey\Uuid\Uuid;
+use Test\Functional\Json;
+use Test\Functional\WebTestCase;
+
+class ConfirmTest extends WebTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadFixtures([
+            ConfirmFixture::class,
+        ]);
+    }
+
+    public function testMethod(): void
+    {
+        $response = $this->app()->handle(self::json('GET', '/v1/auth/join/confirm'));
+
+        self::assertEquals(405, $response->getStatusCode());
+    }
+
+    public function testSuccess(): void
+    {
+        $response = $this->app()->handle(self::json('POST', '/v1/auth/join/confirm', [
+            'token' => ConfirmFixture::VALID,
+        ]));
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals('', (string)$response->getBody());
+    }
+
+    public function testExpired(): void
+    {
+        $response = $this->app()->handle(self::json('POST', '/v1/auth/join/confirm', [
+            'token' => ConfirmFixture::EXPIRED,
+        ]));
+
+        self::assertEquals(409, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        self::assertEquals([
+            'message' => 'Token is expired.',
+        ], Json::decode($body));
+    }
+
+    public function testEmpty(): void
+    {
+        $response = $this->app()->handle(self::json('POST', '/v1/auth/join/confirm', []));
+
+        self::assertEquals(409, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        self::assertEquals([
+            'message' => 'Incorrect token.',
+        ], Json::decode($body));
+    }
+
+    public function testNotExisting(): void
+    {
+        $response = $this->app()->handle(self::json('POST', '/v1/auth/join/confirm', [
+            'token' => Uuid::uuid4()->toString(),
+        ]));
+
+        self::assertEquals(409, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        self::assertEquals([
+            'message' => 'Incorrect token.',
+        ], Json::decode($body));
+    }
+}
