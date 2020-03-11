5ec9669c4c4a5f3c253c548cc77b53c09f4fa8e5
ElisDN <mail@elisdn.ru>
2020-03-11 19:21:02 +0300

    Extracted domain exceptions handling

--- api/config/middleware.php
+++ api/config/middleware.php
@@ -7,6 +7,7 @@ use Slim\App;
 use Slim\Middleware\ErrorMiddleware;
 
 return static function (App $app): void {
+    $app->add(Middleware\DomainExceptionHandler::class);
     $app->add(Middleware\ClearEmptyInput::class);
     $app->addBodyParsingMiddleware();
     $app->add(ErrorMiddleware::class);
--- api/src/Http/Action/V1/Auth/Join/RequestAction.php
+++ api/src/Http/Action/V1/Auth/Join/RequestAction.php
@@ -7,8 +7,6 @@ namespace App\Http\Action\V1\Auth\Join;
 use App\Auth\Command\JoinByEmail\Request\Command;
 use App\Auth\Command\JoinByEmail\Request\Handler;
 use App\Http\EmptyResponse;
-use App\Http\JsonResponse;
-use DomainException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -33,11 +31,8 @@ class RequestAction implements RequestHandlerInterface
         $command->email = $data['email'] ?? '';
         $command->password = $data['password'] ?? '';
 
-        try {
-            $this->handler->handle($command);
-            return new EmptyResponse(201);
-        } catch (DomainException $exception) {
-            return new JsonResponse(['message' => $exception->getMessage()], 409);
-        }
+        $this->handler->handle($command);
+
+        return new EmptyResponse(201);
     }
 }
