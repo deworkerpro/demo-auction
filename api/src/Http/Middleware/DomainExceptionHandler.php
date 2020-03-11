9ab2ac440b62ee059b3d64d5e9f5d2d0e9b9df6d
ElisDN <mail@elisdn.ru>
2020-03-11 19:21:02 +0300

    Extracted domain exceptions handling

---
+++ api/src/Http/Middleware/DomainExceptionHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use App\Http\JsonResponse;
+use DomainException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class DomainExceptionHandler implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            return $handler->handle($request);
+        } catch (DomainException $exception) {
+            return new JsonResponse([
+                'message' => $exception->getMessage(),
+            ], 409);
+        }
+    }
+}
---
+++ api/src/Http/Test/Unit/Middleware/DomainExceptionHandlerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Test\Unit\Middleware;
+
+use App\Http\Middleware\DomainExceptionHandler;
+use DomainException;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Factory\ResponseFactory;
+use Slim\Psr7\Factory\ServerRequestFactory;
+
+/**
+ * @covers DomainExceptionHandler
+ */
+class DomainExceptionHandlerTest extends TestCase
+{
+    public function testNormal(): void
+    {
+        $middleware = new DomainExceptionHandler();
+
+        $handler = $this->createStub(RequestHandlerInterface::class);
+        $handler->method('handle')->willReturn($source = (new ResponseFactory())->createResponse());
+
+        $request = (new ServerRequestFactory())->createServerRequest('POST', 'http://test');
+        $response = $middleware->process($request, $handler);
+
+        self::assertEquals($source, $response);
+    }
+
+    public function testException(): void
+    {
+        $middleware = new DomainExceptionHandler();
+
+        $handler = $this->createStub(RequestHandlerInterface::class);
+        $handler->method('handle')->willThrowException(new DomainException('Some error.'));
+
+        $request = (new ServerRequestFactory())->createServerRequest('POST', 'http://test');
+        $response = $middleware->process($request, $handler);
+
+        self::assertEquals(409, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        /** @var array $data */
+        $data = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
+
+        self::assertEquals([
+            'message' => 'Some error.',
+        ], $data);
+    }
+}
