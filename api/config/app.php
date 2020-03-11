54f3b13c02298d04ee15d9d11e93da075084c8b7
ElisDN <mail@elisdn.ru>
2020-03-11 16:05:49 +0300

    Moved error middleware into container

--- api/config/app.php
+++ api/config/app.php
@@ -8,7 +8,7 @@ use Slim\Factory\AppFactory;
 
 return static function (ContainerInterface $container): App {
     $app = AppFactory::createFromContainer($container);
-    (require __DIR__ . '/middleware.php')($app, $container);
+    (require __DIR__ . '/middleware.php')($app);
     (require __DIR__ . '/routes.php')($app);
     return $app;
 };
