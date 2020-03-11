3c16c22da73edb444f44f89e96e16a0a2dd1b837
ElisDN <mail@elisdn.ru>
2020-03-11 16:59:49 +0300

    Added join request action

--- api/config/routes.php
+++ api/config/routes.php
@@ -2,9 +2,16 @@
 
 declare(strict_types=1);
 
-use App\Http;
+use App\Http\Action;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 
 return static function (App $app): void {
-    $app->get('/', Http\Action\HomeAction::class);
+    $app->get('/', Action\HomeAction::class);
+
+    $app->group('/v1', function (RouteCollectorProxy $group): void {
+        $group->group('/auth', function (RouteCollectorProxy $group): void {
+            $group->post('/join', Action\V1\Auth\Join\RequestAction::class);
+        });
+    });
 };
