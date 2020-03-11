19327a789cb331ea0e612a1daa1c550e40da51eb
ElisDN <mail@elisdn.ru>
2020-03-11 16:57:22 +0300

    Added test json body

--- api/tests/Functional/WebTestCase.php
+++ api/tests/Functional/WebTestCase.php
@@ -12,11 +12,13 @@ use Slim\Psr7\Factory\ServerRequestFactory;
 
 class WebTestCase extends TestCase
 {
-    protected static function json(string $method, string $path): ServerRequestInterface
+    protected static function json(string $method, string $path, array $body = []): ServerRequestInterface
     {
-        return self::request($method, $path)
+        $request = self::request($method, $path)
             ->withHeader('Accept', 'application/json')
             ->withHeader('Content-Type', 'application/json');
+        $request->getBody()->write(json_encode($body, JSON_THROW_ON_ERROR));
+        return $request;
     }
 
     protected static function request(string $method, string $path): ServerRequestInterface
