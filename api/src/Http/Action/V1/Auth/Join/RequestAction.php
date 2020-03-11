d387d653bf891ab579e1ca502766aed21cba4e4c
ElisDN <mail@elisdn.ru>
2020-03-11 18:00:45 +0300

    Added body parse middleware

--- api/src/Http/Action/V1/Auth/Join/RequestAction.php
+++ api/src/Http/Action/V1/Auth/Join/RequestAction.php
@@ -27,7 +27,7 @@ class RequestAction implements RequestHandlerInterface
         /**
          * @psalm-var array{email:?string, password:?string} $data
          */
-        $data = json_decode((string)$request->getBody(), true);
+        $data = $request->getParsedBody();
 
         $command = new Command();
         $command->email = trim($data['email'] ?? '');
