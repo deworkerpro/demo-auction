60eeaa4a7fc0612b99ddcbf175c39fbbc54f1745
ElisDN <mail@elisdn.ru>
2020-03-17 17:47:44 +0300

    Added domain exceptions translation

--- api/config/common/translator.php
+++ api/config/common/translator.php
@@ -48,6 +48,12 @@ return [
                     'ru',
                     'validators'
                 ],
+                [
+                    'php',
+                    __DIR__ . '/../../translations/exceptions.ru.php',
+                    'ru',
+                    'exceptions'
+                ],
             ],
         ],
         'locales' => [
---
+++ api/translations/exceptions.ru.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'User is not found.' => 'Пользователь не найден.',
+    'Confirmation is not required.' => 'Подтверждение не нужно.',
+    'Network is already attached.' => 'Соцсеть уже привязана.',
+    'User is not active.' => 'Пользователь не активен.',
+    'Incorrect token.' => 'Неверный токен.',
+    'User already exists.' => 'Пользователь уже существует.',
+    'Token is not found.' => 'Токен не найден.',
+    'Token is expired.' => 'Время действия токена истекло.',
+    'User does not have an old password.' => 'У пользователя нет старого пароля.',
+    'Resetting is already requested.' => 'Восстановление уже запрошено.',
+    'Resetting is not requested.' => 'Восстановление не запрошено.',
+    'Incorrect current password.' => 'Неверный текущий пароль.',
+    'Email is already same.' => 'Email совпадает с текущим.',
+    'Changing is already requested.' => 'Смена уже запрошена.',
+    'Changing is not requested.' => 'Смена не запрошена.',
+    'Unable to remove active user.' => 'Нельзя удалить активного пользователя.'
+];
