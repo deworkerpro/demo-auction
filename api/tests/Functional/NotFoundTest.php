6e6011bce20956794031f0565c6be0af591c930e
ElisDN <mail@elisdn.ru>
2020-03-17 19:03:51 +0300

    Improved not found page test

--- api/tests/Functional/NotFoundTest.php
+++ api/tests/Functional/NotFoundTest.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace Test\Functional;
 
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+
 class NotFoundTest extends WebTestCase
 {
+    use ArraySubsetAsserts;
+
     public function testNotFound(): void
     {
         $response = $this->app()->handle(self::json('GET', '/not-found'));
 
         self::assertEquals(404, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        /** @var array $data */
+        $data = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
+
+        self::assertArraySubset([
+            'message' => '404 Not Found',
+        ], $data);
     }
 }
