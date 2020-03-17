acab059f5e5b2fb726d40e4b4495b8b9f8c9ee34
ElisDN <mail@elisdn.ru>
2020-03-17 17:39:41 +0300

    Added validation rules

--- api/src/Auth/Command/JoinByEmail/Confirm/Command.php
+++ api/src/Auth/Command/JoinByEmail/Confirm/Command.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Confirm;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command
 {
+    /**
+     * @Assert\NotBlank()
+     */
     public string $token = '';
 }
