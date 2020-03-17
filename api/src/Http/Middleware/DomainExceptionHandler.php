7ca83c0c27f6b883b0e61390a476fcf0f2f65d5b
ElisDN <mail@elisdn.ru>
2020-03-17 19:00:34 +0300

    Added domain errors logging

--- api/src/Http/Middleware/DomainExceptionHandler.php
+++ api/src/Http/Middleware/DomainExceptionHandler.php
@@ -10,14 +10,26 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
 
 class DomainExceptionHandler implements MiddlewareInterface
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         try {
             return $handler->handle($request);
         } catch (DomainException $exception) {
+            $this->logger->warning($exception->getMessage(), [
+                'exception' => $exception,
+                'url' => (string)$request->getUri(),
+            ]);
             return new JsonResponse([
                 'message' => $exception->getMessage(),
             ], 409);
--- api/src/Http/Test/Unit/Middleware/DomainExceptionHandlerTest.php
+++ api/src/Http/Test/Unit/Middleware/DomainExceptionHandlerTest.php
@@ -8,6 +8,7 @@ use App\Http\Middleware\DomainExceptionHandler;
 use DomainException;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
 use Slim\Psr7\Factory\ResponseFactory;
 use Slim\Psr7\Factory\ServerRequestFactory;
 
@@ -18,7 +19,10 @@ class DomainExceptionHandlerTest extends TestCase
 {
     public function testNormal(): void
     {
-        $middleware = new DomainExceptionHandler();
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->never())->method('warning');
+
+        $middleware = new DomainExceptionHandler($logger);
 
         $handler = $this->createStub(RequestHandlerInterface::class);
         $handler->method('handle')->willReturn($source = (new ResponseFactory())->createResponse());
@@ -31,7 +35,10 @@ class DomainExceptionHandlerTest extends TestCase
 
     public function testException(): void
     {
-        $middleware = new DomainExceptionHandler();
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->once())->method('warning');
+
+        $middleware = new DomainExceptionHandler($logger);
 
         $handler = $this->createStub(RequestHandlerInterface::class);
         $handler->method('handle')->willThrowException(new DomainException('Some error.'));
