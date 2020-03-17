3c148ddc51ba34fb092c78d24d5810c81569acf6
ElisDN <mail@elisdn.ru>
2020-03-17 17:39:41 +0300

    Added validation rules

--- api/src/Auth/Command/JoinByEmail/Request/Command.php
+++ api/src/Auth/Command/JoinByEmail/Request/Command.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command
 {
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
     public string $email = '';
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(min=6, allowEmptyString=true)
+     */
     public string $password = '';
 }
