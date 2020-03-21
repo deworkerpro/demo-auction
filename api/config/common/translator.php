3cd30a3785c35ace6808bd64ec884101d47461f8
ElisDN <mail@elisdn.ru>
2020-03-21 10:56:22 +0300

    Migrated to existing middleware

--- api/config/common/translator.php
+++ api/config/common/translator.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Middleware\LocaleNegotiation;
+use Middlewares\ContentLanguage;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Translation\Loader\PhpFileLoader;
 use Symfony\Component\Translation\Loader\XliffFileLoader;
@@ -28,14 +28,14 @@ return [
         return $translator;
     },
 
-    LocaleNegotiation::class => function (ContainerInterface $container): LocaleNegotiation {
+    ContentLanguage::class => function (ContainerInterface $container): ContentLanguage {
         /**
          * @psalm-suppress MixedArrayAccess
          * @psalm-var array{allowed:string[]} $config
          */
         $config = $container->get('config')['locales'];
 
-        return new LocaleNegotiation($config['allowed']);
+        return new ContentLanguage($config['allowed']);
     },
 
     'config' => [
--- api/config/middleware.php
+++ api/config/middleware.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Middleware;
+use Middlewares\ContentLanguage;
 use Slim\App;
 use Slim\Middleware\ErrorMiddleware;
 
@@ -11,7 +12,7 @@ return static function (App $app): void {
     $app->add(Middleware\ValidationExceptionHandler::class);
     $app->add(Middleware\ClearEmptyInput::class);
     $app->add(Middleware\TranslatorLocale::class);
-    $app->add(Middleware\LocaleNegotiation::class);
+    $app->add(ContentLanguage::class);
     $app->addBodyParsingMiddleware();
     $app->add(ErrorMiddleware::class);
 };
