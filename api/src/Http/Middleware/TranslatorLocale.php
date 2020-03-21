7b660c3ba2e94b973dbe748c6d732aa48540df39
ElisDN <mail@elisdn.ru>
2020-03-21 10:51:43 +0300

    Extracted locale negotiation

--- api/src/Http/Middleware/TranslatorLocale.php
+++ api/src/Http/Middleware/TranslatorLocale.php
@@ -8,46 +8,25 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use ptlis\ConNeg\Negotiation;
 use Symfony\Component\Translation\Translator;
 
 class TranslatorLocale implements MiddlewareInterface
 {
     private Translator $translator;
-    private array $locales;
 
-    public function __construct(Translator $translator, array $locales)
+    public function __construct(Translator $translator)
     {
         $this->translator = $translator;
-        $this->locales = $locales;
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $locale = self::parseLocale(
-            $request->getHeaderLine('Accept-Language'),
-            $this->locales
-        );
+        $locale = $request->getHeaderLine('Accept-Language');
 
         if (!empty($locale)) {
             $this->translator->setLocale($locale);
         }
 
         return $handler->handle($request);
-    }
-
-    private static function parseLocale(?string $accept, array $allowed): ?string
-    {
-        /** @var ?string $default */
-        $default = $allowed[0] ?? null;
-
-        if (empty($accept)) {
-            return $default;
-        }
-
-        $negotiation = new Negotiation();
-        $locale = $negotiation->languageBest($accept, implode(',', $allowed));
-
-        return in_array($locale, $allowed, true) ? $locale : $default;
     }
 }
