b908f12f182bbade0f6913632fa1194c17d20384
ElisDN <mail@elisdn.ru>
2020-03-27 12:38:15 +0300

    Extracted welcome component

---
+++ frontend/src/components/Welcome.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Welcome from "./Welcome";
+
+test('renders welcome', () => {
+  const { getByText } = render(<Welcome />);
+  const h1Element = getByText(/Auction/i);
+  expect(h1Element).toBeInTheDocument();
+});
