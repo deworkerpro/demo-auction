763137b94981795c0cad88768ec9702b645c6979
ElisDN <mail@elisdn.ru>
2020-03-27 19:18:41 +0300

    Added home steps drafts

---
+++ cucumber/features/step_definitions/home.js
@@ -0,0 +1,13 @@
+const { Given, When, Then } = require('cucumber');
+
+Given('I am a guest user', function () {
+  return 'pending';
+});
+
+When('I open home page', function () {
+  return 'pending';
+});
+
+Then('I see welcome block', function () {
+  return 'pending';
+});
