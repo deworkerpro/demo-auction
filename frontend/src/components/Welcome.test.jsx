e5c5ba115e2b23d62a2a037353cb89c9f1eb38f2
ElisDN <mail@elisdn.ru>
2020-03-27 13:58:44 +0300

    Fixed JS codestyle

--- frontend/src/components/Welcome.test.jsx
+++ frontend/src/components/Welcome.test.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Welcome from "./Welcome";
+import React from 'react'
+import { render } from '@testing-library/react'
+import Welcome from './Welcome'
 
 test('renders welcome', () => {
-  const { getByText } = render(<Welcome />);
-  const h1Element = getByText(/Auction/i);
-  expect(h1Element).toBeInTheDocument();
-});
+  const { getByText } = render(<Welcome />)
+  const h1Element = getByText(/Auction/i)
+  expect(h1Element).toBeInTheDocument()
+})
