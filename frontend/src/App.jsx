d3b5b3f7e5eb86481909f9b744fa5c55df8f3d3b
ElisDN <mail@elisdn.ru>
2020-03-27 12:38:15 +0300

    Extracted welcome component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="app">
-      <div className="welcome">
-        <h1>Auction</h1>
-        <p>We will be here soon</p>
-      </div>
+      <Welcome />
     </div>
   );
 }
