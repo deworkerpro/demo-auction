e046ad81b547fb64cbfd58c3b1a0348c8d6063af
ElisDN <mail@elisdn.ru>
2020-03-27 12:38:15 +0300

    Extracted welcome component

---
+++ frontend/src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Welcome.css';
+
+function Welcome() {
+  return (
+    <div className="welcome">
+      <h1>Auction</h1>
+      <p>We will be here soon</p>
+    </div>
+  );
+}
+
+export default Welcome;
