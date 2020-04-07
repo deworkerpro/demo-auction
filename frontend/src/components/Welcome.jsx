9a1168bfac7f6d04b331313dc95d13f8a36009f2
ElisDN <mail@elisdn.ru>
2020-04-07 18:39:48 +0300

    Migrated to scoped styles

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Welcome.css';
+import styles from './Welcome.module.css';
 
 function Welcome() {
   return (
-    <div className="welcome">
+    <div className={styles.welcome}>
       <h1>Auction</h1>
       <p>We will be here soon</p>
     </div>
