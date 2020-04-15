2659579331cf6927a71a6503ceebe564ee9a19ef
ElisDN <mail@elisdn.ru>
2020-04-15 13:55:11 +0300

    Added screenshot attach

--- cucumber/features/support/hooks.js
+++ cucumber/features/support/hooks.js
@@ -14,8 +14,8 @@ Before(async function () {
 
 After(async function (testCase) {
   if (testCase.result.status === Status.FAILED) {
-    const name = testCase.sourceLocation.uri + '-' + testCase.sourceLocation.line
-    await this.page.screenshot({ path: 'var/' + name.replace(/\//g, '_') + '.png', fullPage: true })
+    const screenShot = await this.page.screenshot({ encoding: 'base64', fullPage: true })
+    this.attach(screenShot, 'image/png')
   }
   await this.page.close()
   await this.browser.close()
