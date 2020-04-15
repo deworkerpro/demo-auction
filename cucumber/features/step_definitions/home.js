25818aecb0f89bf249ca021000efb98c1f5a3d5e
ElisDN <mail@elisdn.ru>
2020-04-15 14:31:00 +0300

    Extracted page steps

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -1,9 +1,5 @@
-const { When, Then } = require('cucumber')
+const { Then } = require('cucumber')
 const { expect } = require('chai')
-
-When('I open {string} page', async function (uri) {
-  return await this.page.goto('http://gateway:8080' + uri)
-})
 
 Then('I see welcome block', async function () {
   await this.page.waitForSelector('[data-test=welcome]')
