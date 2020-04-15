f300e3c59c35911b80b65f46888c8f7b8961f03b
ElisDN <mail@elisdn.ru>
2020-04-15 13:55:11 +0300

    Added screenshot

--- cucumber/features/support/world.js
+++ cucumber/features/support/world.js
@@ -1,6 +1,7 @@
 const { setWorldConstructor } = require('cucumber')
 
-function CustomWorld () {
+function CustomWorld ({ attach }) {
+  this.attach = attach
   this.browser = null
   this.page = null
 }
