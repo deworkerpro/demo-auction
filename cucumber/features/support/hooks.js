457aed76348eca3ff98bdeb02ef40763602ebfd5
ElisDN <mail@elisdn.ru>
2020-04-15 13:55:11 +0300

    Added screenshot

--- cucumber/features/support/hooks.js
+++ cucumber/features/support/hooks.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer')
-const { Before, After } = require('cucumber')
+const { Before, After, Status } = require('cucumber')
 
 Before(async function () {
   this.browser = await puppeteer.launch({
@@ -12,6 +12,11 @@ Before(async function () {
   await this.page.setViewport({ width: 1280, height: 720 })
 })
 
-After(async function () {
+After(async function (testCase) {
+  if (testCase.result.status === Status.FAILED) {
+    const name = testCase.sourceLocation.uri + '-' + testCase.sourceLocation.line
+    await this.page.screenshot({ path: 'var/' + name.replace(/\//g, '_') + '.png', fullPage: true })
+  }
+  await this.page.close()
   await this.browser.close()
 })
