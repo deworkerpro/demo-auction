717b7cec71bd35cb7f554f985486ae3a37385029
ElisDN <mail@elisdn.ru>
2020-04-15 13:30:42 +0300

    Implemented home welcome checking

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -1,4 +1,5 @@
 const { Given, When, Then } = require('cucumber')
+const { expect } = require('chai')
 
 Given('I am a guest user', function () {})
 
@@ -6,6 +7,8 @@ When('I open home page', async function () {
   return await this.page.goto('http://gateway:8080')
 })
 
-Then('I see welcome block', function () {
-  return 'pending'
+Then('I see welcome block', async function () {
+  await this.page.waitForSelector('[data-test=welcome]')
+  const text = await this.page.$eval('[data-test=welcome] h1', el => el.textContent)
+  expect(text).to.eql('Auction')
 })
