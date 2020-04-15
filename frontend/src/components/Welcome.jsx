a430aa00403742fe1cd861a9502ff04bc2e6dc54
ElisDN <mail@elisdn.ru>
2020-04-15 14:44:19 +0300

    Added data-test attribute

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import styles from './Welcome.module.css'
 
 function Welcome() {
   return (
-    <div className={styles.welcome}>
+    <div data-test="welcome" className={styles.welcome}>
       <h1>Auction</h1>
       <p>We will be here soon</p>
     </div>
