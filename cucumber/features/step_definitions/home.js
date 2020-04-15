a1308670a11f699f0efba6e0863c659920b8df01
ElisDN <mail@elisdn.ru>
2020-04-15 14:30:18 +0300

    Added step argument

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -1,8 +1,8 @@
 const { When, Then } = require('cucumber')
 const { expect } = require('chai')
 
-When('I open home page', async function () {
-  return await this.page.goto('http://gateway:8080')
+When('I open {string} page', async function (uri) {
+  return await this.page.goto('http://gateway:8080' + uri)
 })
 
 Then('I see welcome block', async function () {
