d605331ff77f8ea100e1c2c7c37886677b860769
ElisDN <mail@elisdn.ru>
2020-04-19 15:15:50 +0300

    Added page timeout settings

--- cucumber/features/support/steps/page.js
+++ cucumber/features/support/steps/page.js
@@ -1,5 +1,5 @@
 const { When } = require('cucumber')
 
-When('I open {string} page', async function (uri) {
+When('I open {string} page', { wrapperOptions: { retry: 2 }, timeout: 30000 }, async function (uri) {
   return await this.page.goto('http://gateway:8080' + uri)
 })
