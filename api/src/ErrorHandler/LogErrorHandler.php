b8e749debee86cfee83a5ff06dd2409b020f2598
ElisDN <mail@elisdn.ru>
2020-07-17 16:20:25 +0300

    Updated Slim
    composer update --with-dependencies slim/psr7 slim/slim

--- api/src/ErrorHandler/LogErrorHandler.php
+++ api/src/ErrorHandler/LogErrorHandler.php
@@ -4,27 +4,16 @@ declare(strict_types=1);
 
 namespace App\ErrorHandler;
 
-use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Log\LoggerInterface;
 use Slim\Handlers\ErrorHandler;
-use Slim\Interfaces\CallableResolverInterface;
 
 /**
  * @psalm-suppress PropertyNotSetInConstructor
+ * TODO: Remove $logger after pull request
+ * @property LoggerInterface $logger
  */
 class LogErrorHandler extends ErrorHandler
 {
-    private LoggerInterface $logger;
-
-    public function __construct(
-        CallableResolverInterface $callableResolver,
-        ResponseFactoryInterface $responseFactory,
-        LoggerInterface $logger
-    ) {
-        parent::__construct($callableResolver, $responseFactory);
-        $this->logger = $logger;
-    }
-
     protected function writeToErrorLog(): void
     {
         $this->logger->error($this->exception->getMessage(), [
