abc849790ed76e5add46d7ccb813e0833f3f4b37
ElisDN <mail@elisdn.ru>
2020-07-17 17:21:35 +0300

    Updated migrations

--- api/bin/app.php
+++ api/bin/app.php
@@ -3,8 +3,6 @@
 
 declare(strict_types=1);
 
-use Doctrine\Migrations\Configuration\Configuration;
-use Doctrine\Migrations\Tools\Console\Helper\ConfigurationHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Psr\Container\ContainerInterface;
@@ -34,17 +32,8 @@ $commands = $container->get('config')['console']['commands'];
 
 /** @var EntityManagerInterface $entityManager */
 $entityManager = $container->get(EntityManagerInterface::class);
-$connection = $entityManager->getConnection();
-
-$configuration = new Configuration($connection);
-$configuration->setMigrationsDirectory(__DIR__ . '/../src/Data/Migration');
-$configuration->setMigrationsNamespace('App\Data\Migration');
-$configuration->setMigrationsTableName('migrations');
-$configuration->setAllOrNothing(true);
-$configuration->setCheckDatabasePlatform(false);
 
 $cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
-$cli->getHelperSet()->set(new ConfigurationHelper($connection, $configuration), 'configuration');
 
 foreach ($commands as $name) {
     /** @var Command $command */
---
+++ api/config/common/migrations.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\Migrations;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Tools\Console\Command;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Container\ContainerInterface;
+
+return [
+    DependencyFactory::class => static function (ContainerInterface $container) {
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $container->get(EntityManagerInterface::class);
+
+        $configuration = new Doctrine\Migrations\Configuration\Configuration();
+        $configuration->addMigrationsDirectory('App\Data\Migration', __DIR__ . '/../../src/Data/Migration');
+        $configuration->setAllOrNothing(true);
+        $configuration->setCheckDatabasePlatform(false);
+
+        $storageConfiguration = new Migrations\Metadata\Storage\TableMetadataStorageConfiguration();
+        $storageConfiguration->setTableName('migrations');
+
+        $configuration->setMetadataStorageConfiguration($storageConfiguration);
+
+        return DependencyFactory::fromEntityManager(
+            new Migrations\Configuration\Migration\ExistingConfiguration($configuration),
+            new Migrations\Configuration\EntityManager\ExistingEntityManager($entityManager)
+        );
+    },
+    Command\ExecuteCommand::class => static function (ContainerInterface $container) {
+        /** @var DependencyFactory $factory */
+        $factory = $container->get(DependencyFactory::class);
+        return new Command\ExecuteCommand($factory);
+    },
+    Command\MigrateCommand::class => static function (ContainerInterface $container) {
+        /** @var DependencyFactory $factory */
+        $factory = $container->get(DependencyFactory::class);
+        return new Command\MigrateCommand($factory);
+    },
+    Command\LatestCommand::class => static function (ContainerInterface $container) {
+        /** @var DependencyFactory $factory */
+        $factory = $container->get(DependencyFactory::class);
+        return new Command\LatestCommand($factory);
+    },
+    Command\ListCommand::class => static function (ContainerInterface $container) {
+        /** @var DependencyFactory $factory */
+        $factory = $container->get(DependencyFactory::class);
+        return new Command\ListCommand($factory);
+    },
+    Command\StatusCommand::class => static function (ContainerInterface $container) {
+        /** @var DependencyFactory $factory */
+        $factory = $container->get(DependencyFactory::class);
+        return new Command\StatusCommand($factory);
+    },
+    Command\UpToDateCommand::class => static function (ContainerInterface $container) {
+        /** @var DependencyFactory $factory */
+        $factory = $container->get(DependencyFactory::class);
+        return new Command\UpToDateCommand($factory);
+    },
+    Command\DiffCommand::class => static function (ContainerInterface $container) {
+        /** @var DependencyFactory $factory */
+        $factory = $container->get(DependencyFactory::class);
+        return new Command\DiffCommand($factory);
+    },
+    Command\GenerateCommand::class => static function (ContainerInterface $container) {
+        /** @var DependencyFactory $factory */
+        $factory = $container->get(DependencyFactory::class);
+        return new Command\GenerateCommand($factory);
+    },
+];
