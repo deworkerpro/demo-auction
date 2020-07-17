0b0931de721a576bb537055a37f1a106cab1decc
ElisDN <mail@elisdn.ru>
2020-07-17 17:21:35 +0300

    Updated migrations

--- api/bin/app.php
+++ api/bin/app.php
@@ -4,6 +4,10 @@
 declare(strict_types=1);
 
 use Doctrine\Migrations\Configuration\Configuration;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Configuration\Migration\ExistingConfiguration;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
 use Doctrine\Migrations\Tools\Console\Helper\ConfigurationHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
@@ -34,17 +38,8 @@ $commands = $container->get('config')['console']['commands'];
 
 /** @var EntityManagerInterface $entityManager */
 $entityManager = $container->get(EntityManagerInterface::class);
-$connection = $entityManager->getConnection();
-
-$configuration = new Configuration($connection);
-$configuration->setMigrationsDirectory(__DIR__ . '/../src/Data/Migration');
-$configuration->setMigrationsNamespace('App\Data\Migration');
-$configuration->setMigrationsTableName('migrations');
-$configuration->setAllOrNothing(true);
-$configuration->setCheckDatabasePlatform(false);
 
 $cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
-$cli->getHelperSet()->set(new ConfigurationHelper($connection, $configuration), 'configuration');
 
 foreach ($commands as $name) {
     /** @var Command $command */
--- api/config/common/console.php
+++ api/config/common/console.php
@@ -14,6 +14,7 @@ return [
                 Migrations\Tools\Console\Command\ExecuteCommand::class,
                 Migrations\Tools\Console\Command\MigrateCommand::class,
                 Migrations\Tools\Console\Command\LatestCommand::class,
+                Migrations\Tools\Console\Command\ListCommand::class,
                 Migrations\Tools\Console\Command\StatusCommand::class,
                 Migrations\Tools\Console\Command\UpToDateCommand::class,
             ],
