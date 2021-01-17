c5996a60dc47988f8122cf7e7c7451f7e8f23590
ElisDN <mail@elisdn.ru>
2021-01-17 21:30:50 +0300

    Added features config

--- api/src/FeatureToggle/Features.php
+++ api/src/FeatureToggle/Features.php
@@ -6,8 +6,26 @@ namespace App\FeatureToggle;
 
 class Features implements FeatureFlag
 {
+    /**
+     * @var bool[]
+     * @psalm-param array<string, bool> $features
+     */
+    private array $features;
+
+    /**
+     * @param bool[] $features
+     * @psalm-param array<string, bool> $features
+     */
+    public function __construct(array $features)
+    {
+        $this->features = $features;
+    }
+
     public function isEnabled(string $name): bool
     {
-        return false;
+        if (!array_key_exists($name, $this->features)) {
+            return false;
+        }
+        return $this->features[$name];
     }
 }
