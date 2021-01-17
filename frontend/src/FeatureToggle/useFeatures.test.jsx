450d55b46513519de6ba52e3075b83af35a92c43
ElisDN <mail@elisdn.ru>
2021-01-17 21:19:48 +0300

    Added useFeatures hook

---
+++ frontend/src/FeatureToggle/useFeatures.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import FeaturesProvider from './FeaturesProvider'
+import useFeatures from './useFeatures'
+
+test('read features', () => {
+  const Component = () => {
+    const features = useFeatures()
+    return features.toString()
+  }
+
+  const { container } = render(
+    <FeaturesProvider features={['ONE', 'TWO']}>
+      <Component />
+    </FeaturesProvider>
+  )
+
+  expect(container.textContent).toEqual('ONE,TWO')
+})
