49d310491700eb8a102c73130ae7a6294b776b15
ElisDN <mail@elisdn.ru>
2021-01-17 21:16:00 +0300

    Added feature cookie support

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import cookie from 'cookie'
 
-const features = []
+const defaultFeatures = []
+
+const cookies = cookie.parse(document.cookie)
+const cookieFeatures = (cookies.features || '').split(/\s*,\s*/g)
+
+const features = [...defaultFeatures, ...cookieFeatures]
 
 ReactDOM.render(
   <React.StrictMode>
