99e96c6702c99ea2f850704392e3a49dd72d87e2
ElisDN <mail@elisdn.ru>
2021-01-17 20:09:31 +0300

    Simplified welcome check

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -1,8 +1,5 @@
 const { Then } = require('@cucumber/cucumber')
-const { expect } = require('chai')
 
 Then('I see welcome block', async function () {
   await this.page.waitForSelector('[data-testid=welcome]')
-  const text = await this.page.$eval('[data-testid=welcome] h1', el => el.textContent)
-  expect(text).to.eql('Auction')
 })
