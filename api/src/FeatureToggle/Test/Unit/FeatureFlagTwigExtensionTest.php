087959a97874963f9ff6fb90e0a7da402d7b9a36
ElisDN <mail@elisdn.ru>
2021-01-17 21:36:50 +0300

    Added feature Twig extension

---
+++ api/src/FeatureToggle/Test/Unit/FeatureFlagTwigExtensionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FeatureToggle\Test\Unit;
+
+use App\FeatureToggle\FeatureFlag;
+use App\FeatureToggle\FeatureFlagTwigExtension;
+use PHPUnit\Framework\TestCase;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+
+/**
+ * @covers \App\FeatureToggle\FeatureFlagTwigExtension
+ */
+class FeatureFlagTwigExtensionTest extends TestCase
+{
+    public function testActive(): void
+    {
+        $flag = $this->createMock(FeatureFlag::class);
+        $flag->expects(self::once())->method('isEnabled')->with('ONE')->willReturn(true);
+
+        $twig = new Environment(new ArrayLoader([
+            'page.html.twig' => '<p>{{ is_feature_enabled(\'ONE\') ? \'true\' : \'false\' }}</p>',
+        ]));
+
+        $twig->addExtension(new FeatureFlagTwigExtension($flag));
+
+        self::assertEquals('<p>true</p>', $twig->render('page.html.twig'));
+    }
+
+    public function testNotActive(): void
+    {
+        $flag = $this->createMock(FeatureFlag::class);
+        $flag->expects(self::once())->method('isEnabled')->with('ONE')->willReturn(false);
+
+        $twig = new Environment(new ArrayLoader([
+            'page.html.twig' => '<p>{{ is_feature_enabled(\'ONE\') ? \'true\' : \'false\' }}</p>',
+        ]));
+
+        $twig->addExtension(new FeatureFlagTwigExtension($flag));
+
+        self::assertEquals('<p>false</p>', $twig->render('page.html.twig'));
+    }
+}
