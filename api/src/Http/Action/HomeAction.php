90bcbb1566e71f331412c802ec60a2a474bbe87e
ElisDN <mail@elisdn.ru>
2021-01-17 21:30:50 +0300

    Added API feature toggle

--- api/src/Http/Action/HomeAction.php
+++ api/src/Http/Action/HomeAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Action;
 
+use App\FeatureToggle\FeatureFlag;
 use App\Http\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -12,8 +13,18 @@ use stdClass;
 
 class HomeAction implements RequestHandlerInterface
 {
+    private FeatureFlag $flag;
+
+    public function __construct(FeatureFlag $flag)
+    {
+        $this->flag = $flag;
+    }
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        if ($this->flag->isEnabled('NEW_HOME')) {
+            return new JsonResponse(['name' => 'API']);
+        }
         return new JsonResponse(new stdClass());
     }
 }
