347e845202bd396186548c9cca63b36035e3c49f
ElisDN <mail@elisdn.ru>
2021-02-06 13:49:11 +0300

    Updated dependencies

--- api/config/common/doctrine.php
+++ api/config/common/doctrine.php
@@ -25,7 +25,7 @@ return [
          *     cache_dir:?string,
          *     types:array<string,class-string<Doctrine\DBAL\Types\Type>>,
          *     subscribers:string[],
-         *     connection:array
+         *     connection:array<string, mixed>
          * } $settings
          */
         $settings = $container->get('config')['doctrine'];
