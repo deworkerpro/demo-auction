12cc0be38404fa1489b2bbbd191b01a8c62ca9ab
ElisDN <mail@elisdn.ru>
2021-02-08 13:39:48 +0300

    Implemented join form

--- frontend/src/Join/JoinForm/JoinForm.jsx
+++ frontend/src/Join/JoinForm/JoinForm.jsx
@@ -8,6 +8,10 @@ function JoinForm() {
     agree: false,
   })
 
+  const [errors, setErrors] = useState({})
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(null)
+
   const handleChange = (event) => {
     const input = event.target
     setFormData({
@@ -16,51 +20,132 @@ function JoinForm() {
     })
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (!formData.agree) {
+      setErrors({ agree: 'Please agree with terms.' })
+      return
+    }
+
+    setErrors({})
+    setError(null)
+    setSuccess(null)
+
+    fetch('/api/v1/auth/join', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        email: formData.email,
+        password: formData.password,
+      }),
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          setSuccess('Confirm join by link in email.')
+          return
+        }
+
+        if (response.status === 422) {
+          const data = await response.json()
+          setErrors(data.errors)
+          return
+        }
+
+        const type = response.headers.get('content-type')
+        if (type && type.includes('application/json')) {
+          const data = await response.json()
+          if (data.message) {
+            setError(data.message)
+            return
+          }
+        }
+
+        setError(error.statusText)
+      })
+      .catch((error) => {
+        setError(error.message)
+      })
+  }
+
   return (
     <div data-testid="join-form" className={styles.joinForm}>
-      <form className="form" method="post">
-        <div className="input-row">
-          <label htmlFor="email" className="input-label">
-            Email
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
+      {error ? (
+        <div className="alert error" data-testid="alert-error">
+          {error}
         </div>
-        <div className="input-row">
-          <label htmlFor="password" className="input-label">
-            Password
-          </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
+      ) : null}
+      {success ? (
+        <div className="alert success" data-testid="alert-success">
+          {success}
         </div>
-        <div className="input-row">
-          <label>
+      ) : null}
+
+      {!success ? (
+        <form className="form" method="post" onSubmit={handleSubmit}>
+          <div className={'input-row' + (errors.email ? ' has-error' : '')}>
+            <label htmlFor="email" className="input-label">
+              Email
+            </label>
             <input
-              name="agree"
-              type="checkbox"
-              checked={formData.agree}
+              id="email"
+              name="email"
+              type="email"
+              value={formData.email}
               onChange={handleChange}
               required
             />
-            <small>I agree with privacy policy</small>
-          </label>
-        </div>
-        <div className="button-row">
-          <button type="submit">Join to Us</button>
-        </div>
-      </form>
+            {errors.email ? (
+              <div className="input-error" data-testid="violation">
+                {errors.email}
+              </div>
+            ) : null}
+          </div>
+          <div className={'input-row' + (errors.password ? ' has-error' : '')}>
+            <label htmlFor="password" className="input-label">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            {errors.password ? (
+              <div className="input-error" data-testid="violation">
+                {errors.password}
+              </div>
+            ) : null}
+          </div>
+          <div className={'input-row' + (errors.agree ? ' has-error' : '')}>
+            <label>
+              <input
+                name="agree"
+                type="checkbox"
+                checked={formData.agree}
+                onChange={handleChange}
+                required
+              />
+              <small>I agree with privacy policy</small>
+            </label>
+            {errors.agree ? (
+              <div className="input-error" data-testid="violation">
+                {errors.agree}
+              </div>
+            ) : null}
+          </div>
+          <div className="button-row">
+            <button type="submit" data-testid="join-button">
+              Join to Us
+            </button>
+          </div>
+        </form>
+      ) : null}
     </div>
   )
 }
