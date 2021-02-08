094909e516d99b57ea429e1c4c5dc060202de821
ElisDN <mail@elisdn.ru>
2021-02-08 12:33:24 +0300

    Implemented form steps

---
+++ cucumber/features/support/steps/form.js
@@ -0,0 +1,22 @@
+const { When, Then } = require('@cucumber/cucumber')
+const { expect } = require('chai')
+
+When('I fill {string} field with {string}', async function (name, value) {
+  await this.page.waitForSelector('[name=' + name + ']')
+  await this.page.type('[name=' + name + ']', value)
+})
+
+When('I check {string} checkbox', async function (name) {
+  await this.page.waitForSelector('[name=' + name + ']')
+  await this.page.click('[name=' + name + ']')
+})
+
+Then('I click submit button', async function () {
+  await this.page.click('button[type=submit]')
+})
+
+Then('I see validation error {string}', async function (message) {
+  await this.page.waitForSelector('.input-error')
+  const errors = await this.page.$$eval('.input-error', els => els.map(el => el.innerText))
+  expect(errors.toString()).to.include(message)
+})
