7f0b0e578997bf7581d2457367a9b361993e5b00
ElisDN <mail@elisdn.ru>
2021-02-08 10:43:19 +0300

    Added router

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './App.css'
-import Home from '../Home'
 import { FeaturesProvider } from '../FeatureToggle'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from '../Home'
 
 function App({ features }) {
   return (
     <FeaturesProvider features={features}>
-      <div className="app">
-        <Home />
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </FeaturesProvider>
   )
 }
