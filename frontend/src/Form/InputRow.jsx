ce02834cdcfc1e19cffb13d76faec0574dea32eb
ElisDN <mail@elisdn.ru>
2021-03-02 21:20:56 +0300

    Extracted form components

---
+++ frontend/src/Form/InputRow.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function InputRow({ children, error }) {
+  return (
+    <div className={'input-row' + (error ? ' has-error' : '')}>{children}</div>
+  )
+}
+
+InputRow.propTypes = {
+  children: PropTypes.any,
+  error: PropTypes.string,
+}
+
+export default InputRow
--- frontend/src/Form/index.js
+++ frontend/src/Form/index.js
@@ -1,1 +1,4 @@
+export { default as InputRow } from './InputRow'
+export { default as InputLabel } from './InputLabel'
 export { default as InputError } from './InputError'
+export { default as ButtonRow } from './ButtonRow'
