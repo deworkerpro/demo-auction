4face6681e957515675ce9f920c44af9d2f0b517
ElisDN <mail@elisdn.ru>
2021-04-15 18:01:05 +0300

    Updated deps

--- api/config/common/auth.php
+++ api/config/common/auth.php
@@ -6,15 +6,11 @@ use App\Auth\Entity\User\User;
 use App\Auth\Entity\User\UserRepository;
 use App\Auth\Service\Tokenizer;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
 use Psr\Container\ContainerInterface;
 
 return [
     UserRepository::class => static function (ContainerInterface $container): UserRepository {
         $em = $container->get(EntityManagerInterface::class);
-        /**
-         * @var EntityRepository<User> $repo
-         */
         $repo = $em->getRepository(User::class);
         return new UserRepository($em, $repo);
     },
