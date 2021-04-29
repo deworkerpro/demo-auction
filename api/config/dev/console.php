3cd76bc695a3eca090fcb42c9da7ca715cc456a7
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Added E2E token generation

--- api/config/dev/console.php
+++ api/config/dev/console.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 
 use App\Console\FixturesLoadCommand;
+use App\OAuth\Console\E2ETokenCommand;
 use Doctrine\Migrations;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Command\SchemaTool;
+use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
 use Psr\Container\ContainerInterface;
 
 return [
@@ -22,6 +24,21 @@ return [
         );
     },
 
+    E2ETokenCommand::class => static function (ContainerInterface $container): E2ETokenCommand {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @var array{
+         *    private_key_path:string,
+         * } $config
+         */
+        $config = $container->get('config')['oauth'];
+
+        return new E2ETokenCommand(
+            $config['private_key_path'],
+            $container->get(ClientRepositoryInterface::class)
+        );
+    },
+
     'config' => [
         'console' => [
             'commands' => [
@@ -31,6 +48,8 @@ return [
 
                 Migrations\Tools\Console\Command\DiffCommand::class,
                 Migrations\Tools\Console\Command\GenerateCommand::class,
+
+                E2ETokenCommand::class,
             ],
             'fixture_paths' => [
                 __DIR__ . '/../../src/Auth/Fixture',
---
+++ api/src/OAuth/Console/E2ETokenCommand.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\OAuth\Console;
+
+use App\OAuth\Entity\AccessToken;
+use App\OAuth\Entity\Scope;
+use DateTimeImmutable;
+use League\OAuth2\Server\CryptKey;
+use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+final class E2ETokenCommand extends Command
+{
+    private string $privateKeyPath;
+    private ClientRepositoryInterface $clients;
+
+    public function __construct(
+        string $privateKeyPath,
+        ClientRepositoryInterface $clients
+    ) {
+        parent::__construct();
+        $this->privateKeyPath = $privateKeyPath;
+        $this->clients = $clients;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName('oauth:e2e-token')
+            ->setDescription('Generate E2E-test auth token')
+            ->addArgument('client-id', InputArgument::OPTIONAL)
+            ->addArgument('scopes', InputArgument::OPTIONAL)
+            ->addArgument('user-id', InputArgument::OPTIONAL)
+            ->addArgument('role', InputArgument::OPTIONAL);
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output): void
+    {
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+
+        while (empty($input->getArgument('client-id'))) {
+            $input->setArgument(
+                'client-id',
+                (string)$helper->ask($input, $output, new Question('Client (frontend): ', 'frontend'))
+            );
+        }
+
+        while (empty($input->getArgument('scopes'))) {
+            $input->setArgument(
+                'scopes',
+                (string)$helper->ask($input, $output, new Question('Scopes (common): ', 'common'))
+            );
+        }
+
+        while (empty($input->getArgument('user-id'))) {
+            $input->setArgument(
+                'user-id',
+                (string)$helper->ask($input, $output, new Question('User Id: '))
+            );
+        }
+
+        while (empty($input->getArgument('role'))) {
+            $input->setArgument(
+                'role',
+                (string)$helper->ask($input, $output, new Question('Role (user): ', 'user'))
+            );
+        }
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        /** @var string $clientId */
+        $clientId = $input->getArgument('client-id');
+        /** @var string $scopes */
+        $scopes = $input->getArgument('scopes');
+        /** @var string $userId */
+        $userId = $input->getArgument('user-id');
+        /** @var string $role */
+        $role = $input->getArgument('role');
+
+        $client = $this->clients->getClientEntity($clientId);
+
+        if ($client === null) {
+            $output->writeln('<error>Invalid client ' . $clientId . '</error>');
+            return 1;
+        }
+
+        $token = new AccessToken(
+            $client,
+            array_map(static fn (string $name) => new Scope($name), explode(' ', $scopes))
+        );
+
+        $token->setIdentifier(bin2hex(random_bytes(40)));
+        $token->setExpiryDateTime(new DateTimeImmutable('+1000 years'));
+        $token->setUserIdentifier($userId);
+        $token->setUserRole($role);
+
+        $token->setPrivateKey(new CryptKey($this->privateKeyPath, null, false));
+
+        $output->writeln((string)$token);
+
+        return 0;
+    }
+}
