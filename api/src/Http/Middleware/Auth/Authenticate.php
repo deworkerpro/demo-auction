068b43df4548e420c1000d3fb64675bcf90dda90
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Added auth identity helper

--- api/src/Http/Middleware/Auth/Authenticate.php
+++ api/src/Http/Middleware/Auth/Authenticate.php
@@ -6,6 +6,7 @@ namespace App\Http\Middleware\Auth;
 
 use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\ResourceServer;
+use LogicException;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -14,7 +15,7 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 final class Authenticate implements MiddlewareInterface
 {
-    public const ATTRIBUTE = 'identity';
+    private const ATTRIBUTE = 'identity';
 
     private ResourceServer $server;
     private ResponseFactoryInterface $response;
@@ -23,6 +24,17 @@ final class Authenticate implements MiddlewareInterface
     {
         $this->server = $server;
         $this->response = $response;
+    }
+
+    public static function identity(ServerRequestInterface $request): Identity
+    {
+        $identity = $request->getAttribute(self::ATTRIBUTE);
+
+        if (!$identity instanceof Identity) {
+            throw new LogicException('Unable to fetch identity.');
+        }
+
+        return $identity;
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
