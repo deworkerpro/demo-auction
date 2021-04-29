b304394fd8c6c1275c552578142ae259baaac777
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Added OAuth server

--- api/config/common/oauth.php
+++ api/config/common/oauth.php
@@ -13,6 +13,10 @@ use App\OAuth\Entity\Scope;
 use App\OAuth\Entity\ScopeRepository;
 use App\OAuth\Entity\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\CryptKey;
+use League\OAuth2\Server\Grant\AuthCodeGrant;
+use League\OAuth2\Server\Grant\RefreshTokenGrant;
 use League\OAuth2\Server\Repositories\AccessTokenRepositoryInterface;
 use League\OAuth2\Server\Repositories\AuthCodeRepositoryInterface;
 use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
@@ -23,6 +27,47 @@ use Psr\Container\ContainerInterface;
 use function App\env;
 
 return [
+    AuthorizationServer::class => static function (ContainerInterface $container): AuthorizationServer {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @var array{
+         *    private_key_path:string,
+         *    encryption_key:string,
+         *    auth_code_interval:string,
+         *    access_token_interval:string,
+         *    refresh_token_interval:string,
+         * } $config
+         */
+        $config = $container->get('config')['oauth'];
+
+        $clientRepository = $container->get(ClientRepositoryInterface::class);
+        $scopeRepository = $container->get(ScopeRepositoryInterface::class);
+        $accessTokenRepository = $container->get(AccessTokenRepositoryInterface::class);
+        $authCodeRepository = $container->get(AuthCodeRepositoryInterface::class);
+        $refreshTokenRepository = $container->get(RefreshTokenRepositoryInterface::class);
+
+        $server = new AuthorizationServer(
+            $clientRepository,
+            $accessTokenRepository,
+            $scopeRepository,
+            new CryptKey($config['private_key_path'], null, false),
+            $config['encryption_key']
+        );
+
+        $grant = new AuthCodeGrant(
+            $authCodeRepository,
+            $refreshTokenRepository,
+            new DateInterval($config['auth_code_interval'])
+        );
+        $grant->setRefreshTokenTTL(new DateInterval($config['refresh_token_interval']));
+        $server->enableGrantType($grant, new DateInterval($config['access_token_interval']));
+
+        $grant = new RefreshTokenGrant($refreshTokenRepository);
+        $grant->setRefreshTokenTTL(new DateInterval($config['refresh_token_interval']));
+        $server->enableGrantType($grant, new DateInterval($config['access_token_interval']));
+
+        return $server;
+    },
     ScopeRepositoryInterface::class => static function (ContainerInterface $container): ScopeRepository {
         /**
          * @psalm-suppress MixedArrayAccess
@@ -82,6 +127,12 @@ return [
                     'redirect_uri' => env('FRONTEND_URL') . '/oauth',
                 ],
             ],
+            'encryption_key' => env('JWT_ENCRYPTION_KEY'),
+            'public_key_path' => env('JWT_PUBLIC_KEY_PATH'),
+            'private_key_path' => env('JWT_PRIVATE_KEY_PATH'),
+            'auth_code_interval' => 'PT1M',
+            'access_token_interval' => 'PT10M',
+            'refresh_token_interval' => 'P7D',
         ],
     ],
 ];
