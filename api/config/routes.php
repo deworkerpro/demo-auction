d49bd1bc27ba48c4a8634e03291c6b299a308746
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Added auth identity

--- api/config/routes.php
+++ api/config/routes.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Action;
-use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\Auth\Authenticate;
 use App\Router\StaticRouteGroup as Group;
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
