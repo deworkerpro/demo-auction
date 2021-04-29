2312250bdc4d9992aa9280cd0fa905f96e0f842e
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Added user action

--- api/config/routes.php
+++ api/config/routes.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Action;
+use App\Http\Middleware\Authenticate;
 use App\Router\StaticRouteGroup as Group;
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
@@ -17,6 +18,8 @@ return static function (App $app): void {
         $group->group('/auth', new Group(static function (RouteCollectorProxy $group): void {
             $group->post('/join', Action\V1\Auth\Join\RequestAction::class);
             $group->post('/join/confirm', Action\V1\Auth\Join\ConfirmAction::class);
+
+            $group->get('/user', Action\V1\Auth\UserAction::class)->add(Authenticate::class);
         }));
     }));
 };
---
+++ api/tests/Functional/AuthHeader.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional;
+
+use App\OAuth\Entity\AccessToken;
+use App\OAuth\Entity\Client;
+use App\OAuth\Entity\Scope;
+use DateTimeImmutable;
+use League\OAuth2\Server\CryptKey;
+use function App\env;
+
+final class AuthHeader
+{
+    public static function for(string $userId): string
+    {
+        $token = new AccessToken(
+            new Client(
+                identifier: 'frontend',
+                name: 'Auction',
+                redirectUri: 'http://localhost:8080/oauth'
+            ),
+            [new Scope('common')],
+        );
+        $token->setIdentifier(bin2hex(random_bytes(40)));
+        $token->setExpiryDateTime(new DateTimeImmutable('+1000 years'));
+        $token->setUserIdentifier($userId);
+        $token->setPrivateKey(new CryptKey(env('JWT_PRIVATE_KEY_PATH'), null, false));
+
+        return 'Bearer ' . $token;
+    }
+}
