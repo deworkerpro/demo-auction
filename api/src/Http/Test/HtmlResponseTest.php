a81e41999f6be6ac3f61ae0b317704441e0e0de4
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Added HTML response

---
+++ api/src/Http/Test/HtmlResponseTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Test;
+
+use App\Http\HtmlResponse;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+final class HtmlResponseTest extends TestCase
+{
+    public function testDefault(): void
+    {
+        $response = new HtmlResponse($html = '<html lang="en"></html>');
+
+        self::assertEquals('text/html', $response->getHeaderLine('Content-Type'));
+        self::assertEquals($html, $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testWithCode(): void
+    {
+        $response = new HtmlResponse($html = '<html lang="en"></html>', 201);
+
+        self::assertEquals('text/html', $response->getHeaderLine('Content-Type'));
+        self::assertEquals($html, $response->getBody()->getContents());
+        self::assertEquals(201, $response->getStatusCode());
+    }
+}
