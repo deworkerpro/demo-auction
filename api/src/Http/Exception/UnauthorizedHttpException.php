b63685f787013b43b36c8070045e00d3beece75f
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Made auth optional

---
+++ api/src/Http/Exception/UnauthorizedHttpException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Exception;
+
+use Slim\Exception\HttpUnauthorizedException;
+
+final class UnauthorizedHttpException extends HttpUnauthorizedException
+{
+}
