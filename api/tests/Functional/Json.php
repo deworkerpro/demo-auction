a5cfd23375a75f697d0cc125853dde66c2c3e2bd
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Added authorization code grant tests

--- api/tests/Functional/Json.php
+++ api/tests/Functional/Json.php
@@ -11,4 +11,9 @@ final class Json
         /** @var array */
         return json_decode($data, true, 512, JSON_THROW_ON_ERROR);
     }
+
+    public static function encode(mixed $data): string
+    {
+        return json_encode($data, JSON_THROW_ON_ERROR);
+    }
 }
---
+++ api/tests/Functional/OAuth/AuthorizationCodeFixture.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional\OAuth;
+
+use App\Auth\Entity\User\Id;
+use App\Auth\Test\Builder\UserBuilder;
+use App\OAuth\Entity\AuthCode;
+use App\OAuth\Entity\Client;
+use App\OAuth\Entity\Scope;
+use DateTimeImmutable;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Persistence\ObjectManager;
+
+final class AuthorizationCodeFixture extends AbstractFixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $user = (new UserBuilder())
+            ->withId(new Id('00000000-0000-0000-0000-000000000001'))
+            ->active()
+            ->build();
+
+        $manager->persist($user);
+
+        $code = new AuthCode();
+        $code->setClient(new Client(
+            identifier: 'frontend',
+            name: 'Frontend',
+            redirectUri: 'http://localhost:8080/oauth',
+        ));
+        $code->addScope(new Scope('common'));
+        $code->setExpiryDateTime(new DateTimeImmutable('2300-12-31 21:00:10'));
+        $code->setIdentifier('def50200f204dedbb244ce4539b9e');
+        $code->setUserIdentifier($user->getId()->getValue());
+        $code->setRedirectUri('http://localhost:8080/oauth');
+
+        $manager->persist($code);
+
+        $manager->flush();
+    }
+}
