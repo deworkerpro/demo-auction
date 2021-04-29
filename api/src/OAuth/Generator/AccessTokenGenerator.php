d7fc49804157b5346c09dc1927c17953d3b39767
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Extracted token generator

---
+++ api/src/OAuth/Generator/AccessTokenGenerator.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\OAuth\Generator;
+
+use App\OAuth\Entity\AccessToken;
+use League\OAuth2\Server\CryptKey;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
+
+final class AccessTokenGenerator
+{
+    private string $privateKeyPath;
+
+    public function __construct(string $privateKeyPath)
+    {
+        $this->privateKeyPath = $privateKeyPath;
+    }
+
+    /**
+     * @param ScopeEntityInterface[] $scopes
+     */
+    public function generate(ClientEntityInterface $client, array $scopes, Params $params): AccessToken
+    {
+        $token = new AccessToken($client, $scopes);
+
+        $token->setIdentifier(bin2hex(random_bytes(40)));
+        $token->setExpiryDateTime($params->expires);
+        $token->setUserIdentifier($params->userId);
+        $token->setUserRole($params->role);
+
+        $token->setPrivateKey(new CryptKey($this->privateKeyPath, null, false));
+
+        return $token;
+    }
+}
---
+++ api/src/OAuth/Generator/Params.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\OAuth\Generator;
+
+use DateTimeImmutable;
+
+final class Params
+{
+    public function __construct(
+        public string $userId,
+        public string $role,
+        public DateTimeImmutable $expires,
+    ) {
+    }
+}
