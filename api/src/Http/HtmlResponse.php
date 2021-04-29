265a0f56d5810ebdbae8afa97d585daa994351cb
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Added HTML response

---
+++ api/src/Http/HtmlResponse.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http;
+
+use Slim\Psr7\Factory\StreamFactory;
+use Slim\Psr7\Headers;
+use Slim\Psr7\Response;
+
+final class HtmlResponse extends Response
+{
+    public function __construct(string $html, int $status = 200)
+    {
+        parent::__construct(
+            $status,
+            new Headers(['Content-Type' => 'text/html']),
+            (new StreamFactory())->createStream($html)
+        );
+    }
+}
