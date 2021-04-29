964c71538e311e947329f1702dec6663ff25504c
ElisDN <mail@elisdn.ru>
2021-04-29 13:50:57 +0300

    Added authorize page

--- api/config/routes.php
+++ api/config/routes.php
@@ -10,6 +10,8 @@ use Slim\Routing\RouteCollectorProxy;
 return static function (App $app): void {
     $app->get('/', Action\HomeAction::class);
 
+    $app->map(['GET', 'POST'], '/authorize', Action\AuthorizeAction::class);
+
     $app->group('/v1', new Group(static function (RouteCollectorProxy $group): void {
         $group->group('/auth', new Group(static function (RouteCollectorProxy $group): void {
             $group->post('/join', Action\V1\Auth\Join\RequestAction::class);
---
+++ api/src/Http/Action/AuthorizeAction.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Action;
+
+use App\Http\Response\HtmlResponse;
+use Exception;
+use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
+use Twig\Environment;
+
+final class AuthorizeAction implements RequestHandlerInterface
+{
+    private AuthorizationServer $server;
+    private LoggerInterface $logger;
+    private Environment $template;
+    private ResponseFactoryInterface $response;
+
+    public function __construct(
+        AuthorizationServer $server,
+        LoggerInterface $logger,
+        Environment $template,
+        ResponseFactoryInterface $response
+    ) {
+        $this->server = $server;
+        $this->logger = $logger;
+        $this->template = $template;
+        $this->response = $response;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        try {
+            $authRequest = $this->server->validateAuthorizationRequest($request);
+
+            if ($request->getMethod() === 'POST') {
+                return $this->server->completeAuthorizationRequest($authRequest, $this->response->createResponse());
+            }
+
+            return new HtmlResponse(
+                $this->template->render('authorize.html.twig')
+            );
+        } catch (OAuthServerException $exception) {
+            $this->logger->warning($exception->getMessage(), ['exception' => $exception]);
+            return $exception->generateHttpResponse($this->response->createResponse());
+        } catch (Exception $exception) {
+            $this->logger->error($exception->getMessage(), ['exception' => $exception]);
+            return (new OAuthServerException('Server error.', 0, 'unknown_error', 500))
+                ->generateHttpResponse($this->response->createResponse());
+        }
+    }
+}
