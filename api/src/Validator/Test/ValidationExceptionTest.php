271f9d3dcfcceb09e1d60c0fe20c06832c29fe27
ElisDN <mail@elisdn.ru>
2021-06-20 12:31:49 +0300

    Moved validator

--- api/src/Validator/Test/ValidationExceptionTest.php
+++ api/src/Validator/Test/ValidationExceptionTest.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Test\Validator;
+namespace App\Validator\Test;
 
-use App\Http\Validator\ValidationException;
+use App\Validator\ValidationException;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Validator\ConstraintViolationList;
 
 /**
- * @covers \App\Http\Validator\ValidationException
+ * @covers \App\Validator\ValidationException
  *
  * @internal
  */
