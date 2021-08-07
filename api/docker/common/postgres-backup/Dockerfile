ecc1787691a1f01c4e369e62364864215ecd5c96
ElisDN <mail@elisdn.ru>
2021-08-07 19:07:36 +0300

    Added backup service

---
+++ api/docker/common/postgres-backup/Dockerfile
@@ -0,0 +1,14 @@
+FROM alpine:3.14
+
+RUN apk add --no-cache python3 py3-pip bash coreutils postgresql-client \
+    && pip3 install --upgrade awscli
+
+COPY ./wait-for-it.sh /usr/local/bin/wait-for-it
+RUN chmod 555 /usr/local/bin/wait-for-it
+
+COPY ./postgres-backup/backup.sh /usr/local/bin/backup
+RUN chmod 555 /usr/local/bin/backup
+
+WORKDIR /app
+
+CMD ["backup"]
---
+++ api/docker/common/postgres-backup/backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+if [ -f "$POSTGRES_PASSWORD_FILE" ]; then
+  POSTGRES_PASSWORD="$(cat "$POSTGRES_PASSWORD_FILE")"
+fi
+
+if [ -f "$AWS_SECRET_ACCESS_KEY_FILE" ]; then
+  AWS_SECRET_ACCESS_KEY="$(cat "$AWS_SECRET_ACCESS_KEY_FILE")"
+fi
+
+BACKUP_FILE="${BACKUP_NAME:?}_$(date +%Y-%m-%d_%H-%M).sql.gz"
+
+echo "Dump $BACKUP_FILE"
+
+export PGPASSWORD="${POSTGRES_PASSWORD:?}"
+
+pg_dump \
+    --dbname="${POSTGRES_DB:?}" \
+    --username="${POSTGRES_USERNAME:?}" \
+    --host="${POSTGRES_HOST:?}" \
+    | gzip -9 > "$BACKUP_FILE"
+
+echo "Upload to S3"
+
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:?}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:?}"
+export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:?}"
+
+aws --endpoint-url="${S3_ENDPOINT:?}" s3 cp "$BACKUP_FILE" "s3://${S3_BUCKET:?}/$BACKUP_FILE"
+
+unlink "$BACKUP_FILE"
