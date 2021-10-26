08729354ff49e0384206f26d6777b2e7a54418ea
ElisDN <mail@elisdn.ru>
2021-10-26 16:43:20 +0300

    Removed SwiftMailer

--- api/config/common/mailer.php
+++ api/config/common/mailer.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Finesse\SwiftMailerDefaultsPlugin\SwiftMailerDefaultsPlugin;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcher;
@@ -14,34 +13,6 @@ use Symfony\Component\Mime\Address;
 use function App\env;
 
 return [
-    Swift_Mailer::class => static function (ContainerInterface $container) {
-        /**
-         * @psalm-suppress MixedArrayAccess
-         * @var array{
-         *     host:string,
-         *     port:int,
-         *     user:string,
-         *     password:string,
-         *     encryption:string,
-         *     from:array{email:string, name:string}
-         * } $config
-         */
-        $config = $container->get('config')['mailer'];
-
-        $transport = (new Swift_SmtpTransport($config['host'], $config['port']))
-            ->setUsername($config['user'])
-            ->setPassword($config['password'])
-            ->setEncryption($config['encryption']);
-
-        $mailer = new Swift_Mailer($transport);
-
-        $mailer->registerPlugin(new SwiftMailerDefaultsPlugin([
-            'from' => [$config['from']['email'] => $config['from']['name']],
-        ]));
-
-        return $mailer;
-    },
-
     MailerInterface::class => static function (ContainerInterface $container): MailerInterface {
         /**
          * @psalm-suppress MixedArrayAccess
