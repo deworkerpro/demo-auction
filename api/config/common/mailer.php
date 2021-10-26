227fb7647dc66c1317737d695c15a263dc4c5720
ElisDN <mail@elisdn.ru>
2021-10-26 15:36:30 +0300

    Installed Symfony Mailer

--- api/config/common/mailer.php
+++ api/config/common/mailer.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 use Finesse\SwiftMailerDefaultsPlugin\SwiftMailerDefaultsPlugin;
 use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\Mailer\EventListener\EnvelopeListener;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
+use Symfony\Component\Mime\Address;
 use function App\env;
 
 return [
@@ -16,7 +23,7 @@ return [
          *     user:string,
          *     password:string,
          *     encryption:string,
-         *     from:array
+         *     from:array{email:string, name:string}
          * } $config
          */
         $config = $container->get('config')['mailer'];
@@ -29,20 +36,57 @@ return [
         $mailer = new Swift_Mailer($transport);
 
         $mailer->registerPlugin(new SwiftMailerDefaultsPlugin([
-            'from' => $config['from'],
+            'from' => [$config['from']['email'] => $config['from']['name']],
         ]));
 
         return $mailer;
     },
 
+    MailerInterface::class => static function (ContainerInterface $container): MailerInterface {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @var array{
+         *     host:string,
+         *     port:int,
+         *     user:string,
+         *     password:string,
+         *     encryption:string,
+         *     from:array{email:string, name:string}
+         * } $config
+         */
+        $config = $container->get('config')['mailer'];
+
+        $dispatcher = new EventDispatcher();
+
+        $dispatcher->addSubscriber(new EnvelopeListener(new Address(
+            $config['from']['email'],
+            $config['from']['name']
+        )));
+
+        $transport = (new EsmtpTransport(
+            $config['host'],
+            $config['port'],
+            $config['encryption'] === 'tls',
+            $dispatcher,
+            $container->get(LoggerInterface::class)
+        ))
+            ->setUsername($config['user'])
+            ->setPassword($config['password']);
+
+        return new Mailer($transport);
+    },
+
     'config' => [
         'mailer' => [
             'host' => env('MAILER_HOST'),
-            'port' => env('MAILER_PORT'),
+            'port' => (int)env('MAILER_PORT'),
             'user' => env('MAILER_USERNAME'),
             'password' => env('MAILER_PASSWORD'),
             'encryption' => env('MAILER_ENCRYPTION'),
-            'from' => [env('MAILER_FROM_EMAIL') => 'Auction'],
+            'from' => [
+                'email' => env('MAILER_FROM_EMAIL'),
+                'name' => 'Auction',
+            ],
         ],
     ],
 ];
