318e69d48a6d9896272f39ea944e15ef3ded19c1
ElisDN <mail@elisdn.ru>
2022-04-14 14:13:43 +0300

    Added auth token fetching

--- frontend/src/OAuth/OAuth.js
+++ frontend/src/OAuth/OAuth.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import System from '../Layout/System'
 import { Link } from 'react-router-dom'
+import { AlertError } from '../Alert'
+import useAuth from './Provider/useAuth'
 
 function OAuth() {
+  const { error, loading } = useAuth()
+
   return (
     <System>
       <h1>Auth</h1>
+      <AlertError message={error} />
+      {loading ? <p>Loading...</p> : null}
       <p>
         <Link to="/">Back to Home</Link>
       </p>
