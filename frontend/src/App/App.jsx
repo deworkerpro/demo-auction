b2bcc86d51718e29ec31a4dd6e8bc30b3389a7ed
ElisDN <mail@elisdn.ru>
2022-04-14 14:13:43 +0300

    Added auth provider

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -8,21 +8,34 @@ import Join from '../Join'
 import { NotFound } from '../Error'
 import Confirm from '../Join/Confirm'
 import Success from '../Join/Success'
+import OAuth from '../OAuth'
+import { AuthProvider } from '../OAuth/Provider'
 
 function App({ features }) {
   return (
     <FeaturesProvider features={features}>
-      <BrowserRouter>
-        <div className="app">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/join" element={<Join />} />
-            <Route exact path="/join/confirm" element={<Confirm />} />
-            <Route exact path="/join/success" element={<Success />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <AuthProvider
+        authorizeUrl="/api/authorize"
+        tokenUrl="/api/token"
+        clientId="frontend"
+        scope="common"
+        redirectPath="/oauth"
+      >
+        <BrowserRouter>
+          <div className="app">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              {features.includes('OAUTH') ? (
+                <Route exact path="/oauth" element={<OAuth />} />
+              ) : null}
+              <Route exact path="/join" element={<Join />} />
+              <Route exact path="/join/confirm" element={<Confirm />} />
+              <Route exact path="/join/success" element={<Success />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
     </FeaturesProvider>
   )
 }
