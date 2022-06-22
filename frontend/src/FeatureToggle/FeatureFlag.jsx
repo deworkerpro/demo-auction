73fb576c3631e0752e74dc8443d69fe2c0a57411
ElisDN <mail@elisdn.ru>
2022-06-22 14:45:56 +0300

    Updated frontend deps

--- frontend/src/FeatureToggle/FeatureFlag.jsx
+++ frontend/src/FeatureToggle/FeatureFlag.jsx
@@ -11,6 +11,7 @@ function FeatureFlag({ name, not = false, children }) {
 FeatureFlag.propTypes = {
   name: PropTypes.string.isRequired,
   not: PropTypes.bool,
+  children: PropTypes.object,
 }
 
 export default FeatureFlag
