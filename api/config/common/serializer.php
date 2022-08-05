c5ceecb9383736595baabb069ae5f321467a34f0
ElisDN <mail@elisdn.ru>
2022-08-05 19:12:57 +0300

    Added normalizers

--- api/config/common/serializer.php
+++ api/config/common/serializer.php
@@ -2,8 +2,12 @@
 
 declare(strict_types=1);
 
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -11,7 +15,16 @@ use Symfony\Component\Serializer\SerializerInterface;
 return [
     SerializerInterface::class => static function (): SerializerInterface {
         return new Serializer([
-            new PropertyNormalizer(propertyTypeExtractor: new ReflectionExtractor()),
+            new DateTimeNormalizer(),
+            new PropertyNormalizer(
+                propertyTypeExtractor: new PropertyInfoExtractor(
+                    typeExtractors: [
+                        new PhpDocExtractor(),
+                        new ReflectionExtractor(),
+                    ]
+                )
+            ),
+            new ArrayDenormalizer(),
         ], [
             new JsonEncoder(),
         ]);
