44464c0914cfaa4c31f24a7498f256d2022301bf
ElisDN <mail@elisdn.ru>
2022-08-05 19:20:22 +0300

    Added custom normalizers support

--- api/config/common/serializer.php
+++ api/config/common/serializer.php
@@ -2,19 +2,36 @@
 
 declare(strict_types=1);
 
+use Psr\Container\ContainerInterface;
 use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 return [
-    SerializerInterface::class => static function (): SerializerInterface {
+    SerializerInterface::class => static function (ContainerInterface $container): SerializerInterface {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @var array{
+         *     normalizers: array<array-key, class-string<DenormalizerInterface>|class-string<NormalizerInterface>>
+         * } $config
+         */
+        $config = $container->get('config')['serializer'];
+
+        $normalizers = array_map(
+            static fn (string $name) => $container->get($name),
+            $config['normalizers']
+        );
+
         return new Serializer([
+            ...$normalizers,
             new DateTimeNormalizer(),
             new PropertyNormalizer(
                 propertyTypeExtractor: new PropertyInfoExtractor(
@@ -29,4 +46,10 @@ return [
             new JsonEncoder(),
         ]);
     },
+
+    'config' => [
+        'serializer' => [
+            'normalizers' => [],
+        ],
+    ],
 ];
