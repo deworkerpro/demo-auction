5a7d355346d869e7c316155777b2822466854261
ElisDN <mail@elisdn.ru>
2022-08-05 19:12:57 +0300

    Installed serializer

--- api/src/Http/Response/JsonResponse.php
+++ api/src/Http/Response/JsonResponse.php
@@ -10,12 +10,12 @@ use Slim\Psr7\Response;
 
 final class JsonResponse extends Response
 {
-    public function __construct(mixed $data, int $status = 200)
+    public function __construct(mixed $data, int $status = 200, bool $isJson = false)
     {
         parent::__construct(
             $status,
             new Headers(['Content-Type' => 'application/json']),
-            (new StreamFactory())->createStream(json_encode($data, JSON_THROW_ON_ERROR))
+            (new StreamFactory())->createStream($isJson ? (string)$data : json_encode($data, JSON_THROW_ON_ERROR))
         );
     }
 }
