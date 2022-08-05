8c7b8e99ff735fe147b56063c29c66fb039e8871
ElisDN <mail@elisdn.ru>
2022-08-05 18:43:28 +0300

    Updated Doctrine ORM

--- api/config/common/console.php
+++ api/config/common/console.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 use App\OAuth;
 use Doctrine\Migrations;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+use Psr\Container\ContainerInterface;
 
 return [
+    EntityManagerProvider::class => static fn (ContainerInterface $container): EntityManagerProvider => new SingleManagerProvider($container->get(EntityManagerInterface::class)),
+
+    ValidateSchemaCommand::class => static fn (ContainerInterface $container): ValidateSchemaCommand => new ValidateSchemaCommand($container->get(EntityManagerProvider::class)),
+
     'config' => [
         'console' => [
             'commands' => [
