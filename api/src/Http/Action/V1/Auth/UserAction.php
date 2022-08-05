9988f9b9e0c38b36c45c8cb96b2695f055038820
ElisDN <mail@elisdn.ru>
2022-08-05 21:04:39 +0300

    Reverted native array

--- api/src/Http/Action/V1/Auth/UserAction.php
+++ api/src/Http/Action/V1/Auth/UserAction.php
@@ -7,18 +7,12 @@ namespace App\Http\Action\V1\Auth;
 use App\Http\Exception\UnauthorizedHttpException;
 use App\Http\Middleware\Auth\Authenticate;
 use App\Http\Response\JsonResponse;
-use App\Serializer\Normalizer;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 final class UserAction implements RequestHandlerInterface
 {
-    public function __construct(
-        private readonly Normalizer $normalizer
-    ) {
-    }
-
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $identity = Authenticate::identity($request);
@@ -27,6 +21,9 @@ final class UserAction implements RequestHandlerInterface
             throw new UnauthorizedHttpException($request);
         }
 
-        return new JsonResponse($this->normalizer->normalize($identity));
+        return new JsonResponse([
+            'id' => $identity->id,
+            'role' => $identity->role,
+        ]);
     }
 }
