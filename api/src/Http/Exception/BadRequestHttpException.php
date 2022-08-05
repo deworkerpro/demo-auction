aded0aadd7fc729703033a3517f078820c88d20b
ElisDN <mail@elisdn.ru>
2022-08-05 19:35:48 +0300

    Added invalid format handling

---
+++ api/src/Http/Exception/BadRequestHttpException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Exception;
+
+use Slim\Exception\HttpBadRequestException;
+
+final class BadRequestHttpException extends HttpBadRequestException
+{
+}
