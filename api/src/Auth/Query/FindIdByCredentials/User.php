f491f1fa9aa8f59abdae808cda4141bd10cb93d0
ElisDN <mail@elisdn.ru>
2022-08-05 18:43:28 +0300

    Migrated to native readonly

--- api/src/Auth/Query/FindIdByCredentials/User.php
+++ api/src/Auth/Query/FindIdByCredentials/User.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace App\Auth\Query\FindIdByCredentials;
 
-/**
- * @psalm-immutable
- */
 final class User
 {
     public function __construct(
-        public string $id,
-        public bool $isActive,
+        public readonly string $id,
+        public readonly bool $isActive,
     ) {
     }
 }
--- api/src/Http/Middleware/Auth/Identity.php
+++ api/src/Http/Middleware/Auth/Identity.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware\Auth;
 
-/**
- * @psalm-immutable
- */
 final class Identity
 {
     public function __construct(
-        public string $id,
-        public string $role,
+        public readonly string $id,
+        public readonly string $role,
     ) {
     }
 }
