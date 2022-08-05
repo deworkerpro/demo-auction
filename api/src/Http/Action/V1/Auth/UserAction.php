41aa4dc3f5e3e3dc1bdd328c64fc6900879433b9
ElisDN <mail@elisdn.ru>
2022-08-05 19:48:16 +0300

    Added adapters

--- api/src/Http/Action/V1/Auth/UserAction.php
+++ api/src/Http/Action/V1/Auth/UserAction.php
@@ -7,15 +7,15 @@ namespace App\Http\Action\V1\Auth;
 use App\Http\Exception\UnauthorizedHttpException;
 use App\Http\Middleware\Auth\Authenticate;
 use App\Http\Response\JsonResponse;
+use App\Serializer\Normalizer;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 final class UserAction implements RequestHandlerInterface
 {
     public function __construct(
-        private readonly NormalizerInterface $normalizer
+        private readonly Normalizer $normalizer
     ) {
     }
 
