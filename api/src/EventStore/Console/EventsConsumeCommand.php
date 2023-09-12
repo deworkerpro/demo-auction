9f6a1d11c4ff54f82cbe555955a6b54c087861d3
ElisDN <mail@elisdn.ru>
2023-09-12 17:29:14 +0300

    Add consumed events

--- api/src/EventStore/Console/EventsConsumeCommand.php
+++ api/src/EventStore/Console/EventsConsumeCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\EventStore\Console;
 
+use App\EventStore\Entity\ConsumedEventRepository;
 use App\EventStore\EventListenerResolver\EventListenerResolver;
 use App\EventStore\EventSerializer;
 use App\Queue\Consumer;
@@ -18,6 +19,7 @@ final class EventsConsumeCommand extends Command
     public function __construct(
         private readonly Consumer $consumer,
         private readonly EventSerializer $serializer,
+        private readonly ConsumedEventRepository $consumed,
         private readonly EventListenerResolver $listenerResolver
     ) {
         parent::__construct();
@@ -37,10 +39,15 @@ final class EventsConsumeCommand extends Command
         $output->writeln('<comment>Listen queue ' . $queue . '</comment>');
 
         $this->consumer->consume($queue, function (Message $message) use ($queue, $output): void {
+            if ($this->consumed->hasConsumed($queue, $message->type, $message->id)) {
+                $output->writeln('<info>Skip message ' . $message->type . ':' . $message->id . '</info>');
+                return;
+            }
             $output->writeln('<info>Consume message ' . $message->type . ':' . $message->id . '</info>');
             $event = $this->serializer->unserialize($message->type, $message->payload);
             $listener = $this->listenerResolver->resolve($queue, $event);
             $listener($event);
+            $this->consumed->markAsConsumed($queue, $message->type, $message->id);
         });
 
         $output->writeln('<info>Done!</info>');
