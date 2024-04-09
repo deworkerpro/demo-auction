367ab9cd6e5b0fb3f2253f153156353e0e4d960e
ElisDN <mail@elisdn.ru>
2024-04-09 17:24:44 +0300

    Generated Rector config

---
+++ api/rector.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/translations',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ]);
