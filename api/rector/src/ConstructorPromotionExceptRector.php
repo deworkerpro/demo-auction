781d9b15bac9821d350bf0657911b8c0a3388f74
ElisDN <mail@elisdn.ru>
2024-04-09 20:29:51 +0300

    Implemented Rector

--- api/rector/src/ConstructorPromotionExceptRector.php
+++ api/rector/src/ConstructorPromotionExceptRector.php
@@ -4,53 +4,97 @@ declare(strict_types=1);
 
 namespace App\Rector;
 
-use App\Rector\Tests\ConstructorPromotionExceptRector\ConstructorPromotionExceptRectorTest;
+use Attribute;
 use Override;
 use PhpParser\Node;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Param;
 use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Property;
+use Rector\Contract\Rector\ConfigurableRectorInterface;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Rector\AbstractRector;
-use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Rector\ValueObject\MethodName;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 /**
- * @see ConstructorPromotionExceptRectorTest
+ * @psalm-suppress PropertyNotSetInConstructor
  */
-final class ConstructorPromotionExceptRector extends AbstractRector
+final class ConstructorPromotionExceptRector extends AbstractRector implements ConfigurableRectorInterface
 {
+    public const string EXCEPT_CLASS_ATTRIBUTES = 'except_class_attributes';
+
+    /**
+     * @var list<class-string<Attribute>>
+     */
+    private array $exceptClassAttributes = [];
+
+    public function __construct(
+        private readonly ClassPropertyAssignToConstructorPromotionRector $origin
+    ) {}
+
+    #[Override]
+    public function configure(array $configuration): void
+    {
+        /**
+         * @var array{
+         *     except_class_attributes?: list<class-string<Attribute>>,
+         * } $configuration
+         */
+        $this->exceptClassAttributes = $configuration[self::EXCEPT_CLASS_ATTRIBUTES] ?? [];
+    }
+
     #[Override]
     public function getRuleDefinition(): RuleDefinition
     {
-        return new RuleDefinition('// @todo fill the description', [
-            new CodeSample(
-                <<<'CODE_SAMPLE'
-                    // @todo fill code before
-                    CODE_SAMPLE
-                ,
-                <<<'CODE_SAMPLE'
-                    // @todo fill code after
-                    CODE_SAMPLE
-            ),
-        ]);
+        return new RuleDefinition('Constructor promotion.', []);
     }
 
-    /**
-     * @return array<class-string<Node>>
-     */
     #[Override]
     public function getNodeTypes(): array
     {
-        // @todo select node type
         return [Class_::class];
     }
 
-    /**
-     * @param Class_ $node
-     */
     #[Override]
     public function refactor(Node $node): ?Node
     {
-        // @todo change the node
+        if (!$node instanceof Class_) {
+            return null;
+        }
 
-        return $node;
+        if ($this->exceptClassAttributes !== []) {
+            foreach ($node->attrGroups as $attrGroup) {
+                foreach ($attrGroup->attrs as $attribute) {
+                    if (\in_array($attribute->name->toString(), $this->exceptClassAttributes, true)) {
+                        return null;
+                    }
+                }
+            }
+        }
+
+        $constructClassMethod = $node->getMethod(MethodName::CONSTRUCT);
+        if ($constructClassMethod === null) {
+            return null;
+        }
+
+        $propertiesNames = array_map(
+            static fn (Property $property) => $property->props[0]->name->toString(),
+            $node->getProperties()
+        );
+
+        $constructorParamsNames = array_map(
+            static fn (Param $param) => ($param->var instanceof Variable) ? $param->var->name : '',
+            $constructClassMethod->getParams()
+        );
+
+        sort($propertiesNames);
+        sort($constructorParamsNames);
+
+        if ($propertiesNames !== $constructorParamsNames) {
+            return null;
+        }
+
+        return $this->origin->refactor($node);
     }
 }
--- api/rector/tests/ConstructorPromotionExceptRector/config/configured_rule.php
+++ api/rector/tests/ConstructorPromotionExceptRector/config/configured_rule.php
@@ -3,8 +3,15 @@
 declare(strict_types=1);
 
 use App\Rector\ConstructorPromotionExceptRector;
+use Doctrine\ORM\Mapping\Embeddable;
+use Doctrine\ORM\Mapping\Entity;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(ConstructorPromotionExceptRector::class);
+    $rectorConfig->ruleWithConfiguration(ConstructorPromotionExceptRector::class, [
+        ConstructorPromotionExceptRector::EXCEPT_CLASS_ATTRIBUTES => [
+            Entity::class,
+            Embeddable::class,
+        ],
+    ]);
 };
