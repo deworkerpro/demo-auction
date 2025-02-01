99e5657d8774ed002b32f16ce94399b2b4ac574f
ElisDN <mail@elisdn.ru>
2025-02-01 19:01:07 +0300

    Migrated to endpoint env

--- api/docker/common/postgres-backup/backup.sh
+++ api/docker/common/postgres-backup/backup.sh
@@ -28,7 +28,8 @@ echo "Upload to S3"
 export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:?}"
 export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:?}"
 export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:?}"
+export AWS_ENDPOINT_URL="${S3_ENDPOINT:?}"
 
-aws --endpoint-url="${S3_ENDPOINT:?}" s3 cp "$BACKUP_FILE" "s3://${S3_BUCKET:?}/$BACKUP_FILE"
+aws s3 cp "$BACKUP_FILE" "s3://${S3_BUCKET:?}/$BACKUP_FILE"
 
 unlink "$BACKUP_FILE"
