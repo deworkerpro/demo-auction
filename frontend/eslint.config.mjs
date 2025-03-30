fc63f94b2b42cd0251c7d5ccb04c59c9825f1f2d
ElisDN <mail@elisdn.ru>
2025-03-30 15:24:40 +0300

    Add prettier plugin

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,14 +1,19 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tsEslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import tsEslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default defineConfig([
-  { ignores: ["build/"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { ignores: ['build/'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
   tsEslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-]);
+  pluginPrettierRecommended,
+])
