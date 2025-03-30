be7a658c7c1fb9c07bea6a9895cccbd225de61f7
ElisDN <mail@elisdn.ru>
2025-03-30 16:07:49 +0300

    Migrate to Vite React

--- frontend/jest.config.mjs
+++ frontend/jest.config.mjs
@@ -2,7 +2,27 @@
 export default {
   testEnvironment: 'jsdom',
   transform: {
-    '\\.tsx?$': ['ts-jest', {}],
+    '\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.app.json',
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta',
+              options: {
+                metaObjectReplacement: {
+                  url: 'http://localhost',
+                  env: {
+                    VITE_AUTH_URL: process.env.VITE_AUTH_URL,
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
   },
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
