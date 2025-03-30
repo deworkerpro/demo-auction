a96daa511085deaaa041708ff8f50100fc392530
ElisDN <mail@elisdn.ru>
2025-03-30 15:00:46 +0300

    Remove router futures

--- frontend/src/Join/Success/Success.test.tsx
+++ frontend/src/Join/Success/Success.test.tsx
@@ -5,7 +5,7 @@ import { MemoryRouter } from 'react-router-dom'
 
 test('renders join page', () => {
   render(
-    <MemoryRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <MemoryRouter>
       <Success />
     </MemoryRouter>,
   )
