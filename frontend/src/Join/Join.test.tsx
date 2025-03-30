1e298fd62d99ce5dde9bcfce1649800dfcad2475
ElisDN <mail@elisdn.ru>
2025-03-30 15:00:46 +0300

    Update React Router

--- frontend/src/Join/Join.test.tsx
+++ frontend/src/Join/Join.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Join from './Join'
-import { MemoryRouter } from 'react-router-dom'
+import { MemoryRouter } from 'react-router'
 import FakeAuthProvider from '../OAuth/Provider/FakeAuthProvider'
 
 test('renders join page', () => {
