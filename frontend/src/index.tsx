2c18ff9f09d8dd68c36c3209478dd037d347a28d
ElisDN <mail@elisdn.ru>
2025-03-30 13:46:21 +0300

    Update frontend cookie

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './index.css'
 import App from './App'
-import cookie from 'cookie'
+import { parse } from 'cookie'
 import { mergeFeatures } from './FeatureToggle'
 import defaultFeatures from './features'
 import { createRoot } from 'react-dom/client'
 
-const cookies = cookie.parse(document.cookie)
+const cookies = parse(document.cookie)
 const cookieFeatures = (cookies.features || '').split(/\s*,\s*/g).filter(Boolean)
 
 const features = mergeFeatures(defaultFeatures, cookieFeatures)
