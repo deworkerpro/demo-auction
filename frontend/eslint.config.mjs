e054444affb72e5496434bd5b75cbb7a75af1ac9
ElisDN <mail@elisdn.ru>
2025-03-30 15:36:00 +0300

    Add react-hooks plugin

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,6 +3,7 @@ import globals from 'globals'
 import js from '@eslint/js'
 import tsEslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
 import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default defineConfig([
@@ -15,5 +16,13 @@ export default defineConfig([
   tsEslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat['jsx-runtime'],
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: { 'react-hooks': pluginReactHooks },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+    },
+  },
   pluginPrettierRecommended,
 ])
