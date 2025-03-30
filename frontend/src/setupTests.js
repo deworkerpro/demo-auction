c9b6579dc68d5e6aac71cd89009822ecc5dac6be
ElisDN <mail@elisdn.ru>
2025-03-30 14:29:45 +0300

    Migrate from Enzyme to mocks

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,8 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
-import './jest.polyfil'
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-configure({ adapter: new Adapter() })
