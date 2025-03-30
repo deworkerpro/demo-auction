b88e7d745ee2d597754619acabc431ea4aa1d27c
ElisDN <mail@elisdn.ru>
2025-03-30 16:09:32 +0300

    Add Vite envs

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,11 @@
+interface ViteTypeOptions {
+  strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+  readonly VITE_AUTH_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
